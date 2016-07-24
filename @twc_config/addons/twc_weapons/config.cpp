02e578a7e9f98d9714a4ff1da2dce8851494fa7c
Justin Katulski <jkatulski96@gmail.com>
2016-07-24 15:56:49 -0400

    Fixed cold war helmet in twc config

--- @twc_config/addons/twc_weapons/config.cpp
+++ @twc_config/addons/twc_weapons/config.cpp
@@ -1,3 +1,27 @@
+#define ReadAndWrite		0
+#define ReadAndCreate		1
+#define ReadOnly		2
+#define ReadOnlyVerified		3
+
+#define true	1
+#define false	0
+
+#define VSoft		0
+#define VArmor		1
+#define VAir		2
+
+#define TEast		0
+#define TWest		1
+#define TGuerrila		2
+#define TCivilian		3
+#define TSideUnknown		4
+#define TEnemy		5
+#define TFriendly		6
+#define TLogic		7
+
+#define private		0
+#define protected		1
+#define public		2 
 
 class CfgPatches
 {
@@ -17,12 +41,101 @@ class CfgPatches
 		projectName="TWC";
 		author="FakeMatty";
 	};
+	class SP_Craig_MkBushHelmet
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
+
+class CfgVehicleClasses
+{
+	class SP_MkBushHelmet
+	{
+		displayName = "Mk Bush Helmet";
+	};
 };
 	
 	
 class CfgWeapons
 {
+	class cfgWeapons
+{
+	class ItemCore;
+	class InventoryItem_Base_F;
+	class HeadgearItem;
+
+
+
 	
+
+
+        class SP_MkBushHelmet_Forrest: ItemCore
+	{
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "Mk Bush Helmet (Forrest)";
+		picture = "\twc_weapons\mkbushhelmet\ui\Forrest.jpg";
+		model = "\twc_weapons\Models\MkBushHelmet";
+                hiddenSelectionsTextures[] = {"\twc_weapons\Hats\Mk7Helmet\Green.paa","\twc_weapons\Hats\PASGTHelmet\Black.paa","\twc_weapons\mkbushhelmet\Forrest.paa"};
+                hiddenSelections[] = {"Camo","Camo1","Camo2"};
+                author = "SP Craig";
+                
+                  
+
+		class ItemInfo: HeadgearItem
+		{
+			mass = 60;
+			uniformmodel = "\twc_weapons\Models\MkBushHelmet";
+			modelSides[] = {3,1};
+			armor = 10;
+			passThrough = 0.30;
+                        hiddenSelectionsTextures[] = {"\twc_weapons\Hats\Mk7Helmet\Green.paa","\twc_weapons\Hats\PASGTHelmet\Black.paa","\twc_weapons\mkbushhelmet\Forrest.paa"};
+                        hiddenSelections[] = {"Camo","Camo1","Camo2"};
+
+
+
+		};
+	};
+
+
+
+        class SP_MkBushHelmet_Tropical: ItemCore
+	{
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "Mk Bush Helmet (Tropical)";
+		picture = "\twc_weapons\mkbushhelmet\ui\Tropical.jpg";
+		model = "\twc_weapons\Models\MkBushHelmet";
+                hiddenSelectionsTextures[] = {"\twc_weapons\Hats\Mk7Helmet\Tan.paa","\twc_weapons\Hats\PASGTHelmet\Black.paa","\twc_weapons\mkbushhelmet\Tropical.paa"};
+                hiddenSelections[] = {"Camo","Camo1","Camo2"};
+                author = "SP Craig";
+                
+                  
+
+		class ItemInfo: HeadgearItem
+		{
+			mass = 60;
+			uniformmodel = "\twc_weapons\Models\MkBushHelmet";
+			modelSides[] = {3,1};
+			armor = 10;
+			passThrough = 0.30;
+                        hiddenSelectionsTextures[] = {"\twc_weapons\Hats\Mk7Helmet\Tan.paa","\twc_weapons\Hats\PASGTHelmet\Black.paa","\twc_weapons\mkbushhelmet\Tropical.paa"};
+                        hiddenSelections[] = {"Camo","Camo1","Camo2"};
+
+
+
+
+
+
+
+
+		};
+	};
+};
+
 	class UK3CB_BAF_L85A2;
 	class UK3CB_BAF_L85A2_RIS_AFG;
 	class UK3CB_BAF_L85A2_RIS;
