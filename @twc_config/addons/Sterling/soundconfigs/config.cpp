18df5c6cc30a3221559365db65edd1d8adf3870c
Neciota <neciota@gmail.com>
2017-02-08 01:28:18 +0100

    Removed sterling/scimitar

--- @twc_config/addons/Sterling/soundconfigs/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "cfgpatches.hpp"
-
-#include "cfgsoundcurves.hpp" // attenuation curves
-#include "cfgsoundshaders.hpp" // SoundShaders
-#include "cfgsoundsets.hpp" // SoundSets
-#include "cfgsound3dprocessors.hpp" // 3D processors configuration
-#include "cfgsoundfilters.hpp" // DSP filters configuration
