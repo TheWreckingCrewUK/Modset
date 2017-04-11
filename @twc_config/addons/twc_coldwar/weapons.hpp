61aa41b0ac8af6f2274d699d68fb88c2e2087e68
Neciota <neciota@gmail.com>
2017-04-11 20:40:40 +0200

    Fixed dummkopf mistake
    Never fuck your inheritances up kids.

--- @twc_config/addons/twc_coldwar/weapons.hpp
+++ @twc_config/addons/twc_coldwar/weapons.hpp
@@ -38,7 +38,8 @@ class cfgWeapons
 		author="Jayman";
 		displayName="Stinger A";
 	};
-	class ukcw_l4
+	class CUP_arifle_RPK74;
+	class ukcw_l4: CUP_arifle_RPK74
 	{
 		magazines[] = {"ukcw_30Rnd_762x51_STANAG","CUP_20Rnd_762x51_FNFAL_M","UK3CB_BAF_762_20Rnd"};		
 	};
