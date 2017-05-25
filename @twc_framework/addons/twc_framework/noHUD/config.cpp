2cc2b6e164a31da79968a70d5497cb566c7ed8f6
Bosenator <strooptroopers+ghbose@gmail.com>
2017-05-25 15:56:26 +0100

    Override Default HUD (WIP)

---
+++ @twc_framework/addons/twc_framework/noHUD/config.cpp
@@ -0,0 +1,64 @@
+class CfgPatches
+{
+	class NoDefaultHUD
+	{
+		name = "No Default HUD";
+		author = "Bosenator";
+		url = "http://thewreckingcrew.eu";
+		
+		requiredVersion = 1.70;
+	}
+}
+
+class DefaultVehicleSystemsDisplayManagerLeft
+{
+	componentType = "VehicleSystemsDisplayManager"; //mandatory
+	x = (safezoneX + 0.5 * (((safezoneW / safezoneH) min 1.2) / 40));
+	y = (safezoneY + safezoneH - 21 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25));
+	left = 1;
+	forcedDisplay = "EmptyDisplay";
+
+	class Components
+	{
+		class MinimapDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+		
+		class CrewDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+		
+		class EmptyDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+	};
+};
+
+class DefaultVehicleSystemsDisplayManagerRight
+{
+	x = ((10 * (((safezoneW / safezoneH) min 1.2) / 40)) + 0.5 * (((safezoneW / safezoneH) min 1.2) / 40)));
+	left = 0;
+	right = 1;
+	forcedDisplay = "EmptyDisplay";
+
+	class Components
+	{
+		class MinimapDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+		
+		class CrewDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+		
+		class EmptyDisplay
+		{
+			componentType = "EmptyDisplayComponent";
+		};
+	};
+};
