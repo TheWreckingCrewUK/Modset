d7fd8b1c927df64e81f6b3a28357d789e4f3fd35
Neciota <neciota@gmail.com>
2017-09-13 03:13:32 +0200

    L96A1
    L96A1 included for '90s sniper goyims.
    
    Class:
    twc_l96_w
    twc_l96_d

--- @twc_config/addons/twc_weapons/config.cpp
+++ @twc_config/addons/twc_weapons/config.cpp
@@ -75,4 +75,34 @@ class cfgWeapons
 			mass=220;
 		};
 	};
+	class UK3CB_BAF_L115A3;
+	class UK3CB_BAF_L115A3_DE;
+	class twc_l96_w: UK3CB_BAF_L115A3
+	{
+		scope = 1;
+		displayname = "L96A1";
+		magazines[] = {"UK3CB_BAF_762_L42A1_10Rnd"};
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="CUP_optic_Leupold_VX3";
+			};
+		};
+	};
+	class twc_l96_d: UK3CB_BAF_L115A3_DE
+	{
+		scope = 1;
+		displayname = "L96A1";
+		magazines[] = {"UK3CB_BAF_762_L42A1_10Rnd"};
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="CUP_optic_Leupold_VX3";
+			};
+		};
+	};
 };
