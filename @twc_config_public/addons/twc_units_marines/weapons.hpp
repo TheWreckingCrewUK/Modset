62915b4daaf23f4aee0da0bfc448a12fe818017f
Justin Katulski <jkatulski96@gmail.com>
2017-10-26 16:42:41 -0400

    charlie weapons

---
+++ @twc_config_public/addons/twc_units_marines/weapons.hpp
@@ -0,0 +1,75 @@
+class cfgWeapons
+{
+	class rhs_weap_m4a1_m203;
+	class rhs_weap_m4a1;
+	class rhs_weap_m249_pip_L;
+	class rhs_weap_m240B;
+	
+	class twc_rhs_weap_m4a1_m203_acog_lazer: rhs_weap_m4a1_m203
+	{
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="rhsusf_acc_ACOG";
+			};
+			class LinkedItemsFrontSideRail
+			{
+				slot="PointerSlot";
+				item="rhsusf_acc_anpeq15A";
+			};
+		};
+	};
+	
+	class twc_rhs_weap_m4a1_acog_lazer: rhs_weap_m4a1
+	{
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="rhsusf_acc_ACOG";
+			};
+			class LinkedItemsFrontSideRail
+			{
+				slot="PointerSlot";
+				item="rhsusf_acc_anpeq15A";
+			};
+		};
+	};
+	
+	class twc_rhs_weap_m249_pip_L_mgo_lazer: rhs_weap_m249_pip_L
+	{
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="rhsusf_acc_ELCAN";
+			};
+			class LinkedItemsFrontSideRail
+			{
+				slot="PointerSlot";
+				item="rhsusf_acc_anpeq15A";
+			};
+		};
+	};
+	
+	class twc_rhs_weap_m240B_mgo_lazer: rhs_weap_m240B
+	{
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="rhsusf_acc_ELCAN";
+			};
+			class LinkedItemsFrontSideRail
+			{
+				slot="PointerSlot";
+				item="rhsusf_acc_anpeq15A";
+			};
+		};
+	};
+};
