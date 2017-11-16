cb7a98238fcd5ffbfe6ee04911f89577f0773c62
Bosenator <strooptroopers+ghbose@gmail.com>
2017-11-16 17:16:25 +0000

    modern #defines

--- @twc_config/addons/twc_vehicles/modern/modern.hpp
+++ @twc_config/addons/twc_vehicles/modern/modern.hpp
@@ -2,29 +2,29 @@
 #undef ADD_FACTION
 #define ADD_FACTION faction = "TWC_Modern"
 
-// Classname, Amount Per Section
-#define MODERN_RIFLE ["UK3CB_BAF_556_30Rnd", 24]
-#define MODERN_RIFLE_T ["UK3CB_BAF_556_30Rnd_T", 4]
+// Amount Per Section
+#define MODERN_RIFLE 24
+#define MODERN_RIFLE_T 4
 
-#define MODERN_SHOTGUN_SLUG ["UK3CB_BAF_12G_Slugs", 4]
-#define MODERN_SHOTGUN_BUCK ["UK3CB_BAF_12G_Pellets", 4]
+#define MODERN_SHOTGUN_SLUG 4
+#define MODERN_SHOTGUN_BUCK 4
 
-#define MODERN_UGL_HE 
-#define MODERN_UGL_RED 
-#define MODERN_UGL_BLUE 
-#define MODERN_UGL_WHITE 
+#define MODERN_UGL_HE 6
+#define MODERN_UGL_RED 2
+#define MODERN_UGL_BLUE 2
+#define MODERN_UGL_WHITE 2
 
-#define MODERN_AUTORIFLE 
-#define MODERN_AUTORIFLE_T 
+#define MODERN_AUTORIFLE 4
+#define MODERN_AUTORIFLE_T 2
 
-#define MODERN_MECHA_AUTORIFLE 
-#define MODERN_MECHA_AUTORIFLE_T
+#define MODERN_MECHA_AUTORIFLE 2
+#define MODERN_MECHA_AUTORIFLE_T 1
 
-#define MODERN_MARKSMAN ["UK3CB_BAF_762_L42A1_20Rnd", 5]
-#define MODERN_MARKSMAN_T 
+#define MODERN_MARKSMAN 6
+#define MODERN_MARKSMAN_T 2
 
-#define MODERN_MACHINEGUNNER 
-#define MODERN_MACHINEGUNNER_T 
+#define MODERN_MACHINEGUNNER 2
+#define MODERN_MACHINEGUNNER_T 1
 
 // Modifiers to the total amount (used to alter different vehicle payloads)
 #define MODERN_MASTIFF_MOD 2
