22cd9be9ca64ae117d06adc2a7c377aa25815c3f
Bosenator <strooptroopers+ghbose@gmail.com>
2017-12-12 23:43:33 +0000

    base_defence adjustments

--- @twc_config_public/addons/twc_basedefence/config.cpp
+++ @twc_config_public/addons/twc_basedefence/config.cpp
@@ -1,7 +1,9 @@
-class CfgPatches 
-{
-	class BRIDGE_ArmEn_siren
-	{
+/*
+ * Author: [TWC] Hobbs
+ * Descri: Mortar defence system & 
+ */
+class CfgPatches {
+	class TWC_Public_Siren {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.100000;
@@ -11,46 +13,41 @@ class CfgPatches
 	};
 };
 
-
-
-class CfgFunctions 
-{
-	class TWC
-	{
-		class idf_Functions
-		{
+class CfgFunctions {
+	class TWC {
+		class idf_Functions {
 			file = "\twc_basedefence\functions";
-			class init{postInit = 1;};
+
+			class init {
+				postInit = 1;
+			};
 		};
 	};
 };
 
 class CfgVehicles {
+	class staticweapon;
+	class staticmortar: staticweapon {
 
-class staticweapon;
-  class staticmortar: staticweapon {
-
-	class EventHandlers{
-   fired="[_this select 0, _this select 6] execVM ""insurgency_core\server\sys_basedefence\IDF_Alarmfire.sqf""";
-};
-
+		class EventHandlers {
+			fired = "[(_this select 0), (_this select 6)] execVM ""insurgency_core\server\sys_basedefence\IDF_Alarmfire.sqf""";
+		};
 	};
 };
 
-class CfgSounds
-{
-	class TWC_sound_idfsiren
-	{
+class CfgSounds {
+	sounds[] = {TWC_sound_idfsiren, TWC_sound_idfclear};
+
+	class TWC_sound_idfsiren {
 		name = "TWC_sound_idfsiren";
-		sound[] = {"\sounds\idfalarm.wav", 2, 1};
-		titles[] = {};
+		// filename, volume, pitch, distance
+		sound[] = {"\twc_basedefence\sounds\idfalarm.ogg", 2, 1, 500};
+		titles[] = {0, "IDF SIREN"};
 	};
 	
-	class TWC_sound_idfclear
-	{
+	class TWC_sound_idfclear {
 		name = "TWC_sound_idfclear";
-		sound[] = {"\sounds\idfclear.wav", 2, 1};
-		titles[] = {};
+		sound[] = {"\twc_basedefence\sounds\idfclear.ogg", 2, 1, 500};
+		titles[] = {0, "IDF CLEAR"};
 	};
 };
-
