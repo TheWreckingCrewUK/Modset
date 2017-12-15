f4c983b7b54e238015b85c173524095e9b32e51b
Hobbnob <thehobbnob@gmail.com>
2017-12-15 14:46:27 +0000

    ammobox rebalancing now that SL's can spawn small boxes

--- @twc_config_public/addons/twc_ammoboxes/config.cpp
+++ @twc_config_public/addons/twc_ammoboxes/config.cpp
@@ -56,52 +56,52 @@ class CfgVehicles
 			class _xx_stanag
 			{
 				magazine = "UK3CB_BAF_556_30Rnd";
-				count = 20;
+				count = 30;
 			};
 			class _xx_stanagTracer
 			{
 				magazine = "UK3CB_BAF_556_30Rnd_T";
-				count = 5;
+				count = 15;
 			};
 			class _xx_stanag_box
 			{
 				magazine = "UK3CB_BAF_556_200Rnd";
-				count = 4;
+				count = 10;
 			};
 			class _xx_stanag_boxTracer
 			{
 				magazine ="UK3CB_BAF_556_200Rnd_T";
-				count = 2;
+				count = 5;
 			};
 			class _xx_762_belt
 			{
 				magazine = "UK3CB_BAF_762_100Rnd";
-				count = 2;
+				count = 10;
 			};
 			class _xx_762_beltTracer
 			{
 				magazine = "UK3CB_BAF_762_100Rnd_T";
-				count = 1;
+				count = 3;
 			};
 			class _xx_762_mag
 			{
 				magazine = "UK3CB_BAF_762_L42A1_20Rnd";
-				count = 2;
+				count = 10;
 			};
 			class _xx_762_magTracer
 			{
 				magazine = "UK3CB_BAF_762_L42A1_20Rnd_T";
-				count = 1;
+				count = 4;
 			};
 			class _xx_1rnd_he
 			{
 				magazine = "1Rnd_HE_Grenade_shell";
-				count = 10;
+				count = 20;
 			};
 			class _xx_1rnd_smoke
 			{
 				magazine = "1Rnd_Smoke_Grenade_shell";
-				count = 2;
+				count = 4;
 			};
 			class _xx_handgrenade
 			{
@@ -119,32 +119,37 @@ class CfgVehicles
 			class _xx_fieldDressing
 			{
 				name = "ACE_fieldDressing";
-				count = 10;
+				count = 15;
 			};
 			class _xx_packingBandage
 			{
 				name = "ACE_packingBandage";
-				count = 10;
+				count = 15;
 			};
 			class _xx_elasticBandage
 			{
 				name = "ACE_elasticBandage";
-				count = 10;
+				count = 15;
 			};
 			class quickclot
 			{
 				name = "ACE_quickclot";
-				count = 10;
+				count = 15;
 			};
 			class morphine
 			{
 				name = "ACE_morphine";
-				count = 5;
+				count = 10;
 			};
 			class epinephrine
 			{
 				name = "ACE_epinephrine";
 				count = 2;
+			};
+						class saline
+			{
+				name = "ACE_salineIV_500";
+				count = 6;
 			};
 		};
 		class transportbackpacks
@@ -173,23 +178,23 @@ class CfgVehicles
 		{
 			class _xx_stanag
 			{
-				magazine = "UK3CB_BAF_556_30Rnd";
+				magazine = "30Rnd_556x45_Stanag";
 				count = 30;
 			};
 			class _xx_stanagTracer
 			{
-				magazine = "UK3CB_BAF_556_30Rnd_T";
+				magazine = "30Rnd_556x45_Stanag_Tracer_Red";
 				count = 10;
 			};
 			class _xx_stanag_box
 			{
 				magazine = "rhsusf_200Rnd_556x45_soft_pouch";
-				count = 6;
+				count = 10;
 			};
 			class _xx_762_box
 			{
 				magazine = "rhsusf_100Rnd_762x51";
-				count = 5;
+				count = 7;
 			};
 			class _xx_762_boxTracer
 			{
@@ -199,7 +204,7 @@ class CfgVehicles
 			class _xx_1rnd_he
 			{
 				magazine = "1Rnd_HE_Grenade_shell";
-				count = 10;
+				count = 20;
 			};
 			class _xx_1rnd_smoke
 			{
@@ -222,22 +227,22 @@ class CfgVehicles
 			class _xx_fieldDressing
 			{
 				name = "ACE_fieldDressing";
-				count = 10;
+				count = 15;
 			};
 			class _xx_packingBandage
 			{
 				name = "ACE_packingBandage";
-				count = 10;
+				count = 15;
 			};
 			class _xx_elasticBandage
 			{
 				name = "ACE_elasticBandage";
-				count = 10;
+				count = 15;
 			};
 			class quickclot
 			{
 				name = "ACE_quickclot";
-				count = 10;
+				count = 15;
 			};
 			class morphine
 			{
@@ -248,6 +253,11 @@ class CfgVehicles
 			{
 				name = "ACE_epinephrine";
 				count = 2;
+			};
+									class saline
+			{
+				name = "ACE_salineIV_500";
+				count = 6;
 			};
 		};
 		class transportbackpacks
