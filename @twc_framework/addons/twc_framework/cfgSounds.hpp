98e47d238d50cbc754a6abb73106ba9cb99213d1
Bosenator <strooptroopers+ghbose@gmail.com>
2017-12-28 12:33:57 +0000

    op whirlwind specifics

---
+++ @twc_framework/addons/twc_framework/cfgSounds.hpp
@@ -0,0 +1,14 @@
+class CfgSounds
+{
+	class intro_music {
+		name = "intro_music"
+		sound[] = {"twc_framework\music\intro.ogg", db + 5, 1.0 };
+		titles[] = {0, ""};
+	};
+	
+	class outro_music {
+		name = "outro_music"
+		sound[] = {"twc_framework\music\outro.ogg", db + 5, 1.0 };
+		titles[] = {0, ""};
+	};
+};
