86169b2303784f719991c418090bc51c637ade8c
Bosenator <strooptroopers+ghbose@gmail.com>
2017-12-28 12:47:59 +0000

    missing semis

--- @twc_framework/addons/twc_framework/cfgSounds.hpp
+++ @twc_framework/addons/twc_framework/cfgSounds.hpp
@@ -1,13 +1,13 @@
 class CfgSounds
 {
 	class intro_music {
-		name = "intro_music"
+		name = "intro_music";
 		sound[] = {"twc_framework\music\intro.ogg", db + 5, 1.0 };
 		titles[] = {0, ""};
 	};
 	
 	class outro_music {
-		name = "outro_music"
+		name = "outro_music";
 		sound[] = {"twc_framework\music\outro.ogg", db + 5, 1.0 };
 		titles[] = {0, ""};
 	};
