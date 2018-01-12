2899db1b9849eb7084501a68f761c6d5ee6d9458
Justin Katulski <jkatulski96@gmail.com>
2018-01-12 18:49:29 -0500

    Now it is fixed

--- @twc_framework/addons/twc_framework/cfgWeapons.hpp
+++ @twc_framework/addons/twc_framework/cfgWeapons.hpp
@@ -1,14 +1,14 @@
 class cfgWeapons
 {
 	class ItemCore;
-	class InventoryItem_Base_F;
+	class CBA_MiscItem_ItemInfo;
 	class ACE_ItemCore;
 	class TWC_Syrette: ACE_ItemCore
 	{
 		scope = 2;
 		displayName = "Morphine Syrette";
 		descriptionShort = "Used to combat moderate to severe pain experiences";
-		class ItemInfo: InventoryItem_Base_F
+		class ItemInfo: CBA_MiscItem_ItemInfo
 		{
 			mass = 1;
 		};
