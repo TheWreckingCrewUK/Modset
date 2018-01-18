ad9ad31466e030e2c190dd79ee80011f9ffd1d1d
Bosenator <strooptroopers+ghbose@gmail.com>
2018-01-18 23:01:04 +0000

    define icon

--- @twc_framework/addons/twc_framework/cfgWaypoints.hpp
+++ @twc_framework/addons/twc_framework/cfgWaypoints.hpp
@@ -5,7 +5,9 @@ class cfgWaypoints {
 		//--- Waypoint class
 		class wpMove20MPH {
 			displayName = "Move at 20mph";
+			displayNameDebug = "wpMove20MPH";
 			file = "twc_framework\waypoints\fn_wpMove20MPH.sqf";
+			icon = "\a3\3den\Data\CfgWaypoints\getInNearest_ca.paa";
 		};
 	};
 };
