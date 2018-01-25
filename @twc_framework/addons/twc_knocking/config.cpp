ececdf62a909650ba3d624754c580bee4ce97290
Bosenator <strooptroopers+ghbose@gmail.com>
2018-01-25 21:58:15 +0000

    TWC_Knocking CLean Up. No idea why sound is bork.

--- @twc_framework/addons/twc_knocking/config.cpp
+++ @twc_framework/addons/twc_knocking/config.cpp
@@ -1,22 +1,17 @@
-class CfgPatches 
-{
-	class BRIDGE_ArmEn_Knocking
-	{
+class CfgPatches {
+	class TWC_Knocking {
 		units[] = {};
 		weapons[] = {};
-		requiredVersion = 0.100000;
+		requiredVersion = 0.1;
 		requiredAddons[] = {"ace_interaction"};
 		author[] = {""};
 		authorUrl = "";
 	};
 };
 
-class CfgFunctions 
-{
-	class TWC
-	{
-		class Knock_Functions
-		{
+class CfgFunctions {
+	class TWC {
+		class Knock_Functions {
 			file = "\twc_knocking\functions";
 			class init{postInit = 1;};
 		};
@@ -25,18 +20,17 @@ class CfgFunctions
 
 
 class CfgVehicles {
-
-  class LandVehicle;
-  class Tank: LandVehicle {
-    class ACE_Actions {
+	class LandVehicle;
+	class Tank: LandVehicle {
+	class ACE_Actions {
 			class ACE_MainActions {
 				class TWC_knock {
 					displayName = "Knock";
 					condition = "vehicle player == player";
-					statement = "_this call TWC_fnc_knockOnTank";
+					statement = "[_target] call TWC_fnc_knockOnTank;";
 					showDisabled = 0;
 					priority = 1;
-					distance = 4.500000;
+					distance = 4.5;
 					icon = "\twc_knocking\ui\knock.paa";
 				};
 			};
@@ -44,18 +38,16 @@ class CfgVehicles {
 	};
 };
 
-class CfgSounds
-{
+class CfgSounds {
 	sounds[] = {TWC_sound_knockMetal};
-	class TWC_sound_knockMetal
-	{
+
+	class TWC_sound_knockMetal {
 		name = "TWC_sound_knockMetal";
 		sound[] = {"\twc_knocking\sounds\knockMetal.ogg", 2, 1};
 		titles[] = {};
 	};
-	
-	class TWC_sound_knockMetalInside
-	{
+
+	class TWC_sound_knockMetalInside {
 		name = "TWC_sound_knockMetalInside";
 		sound[] = {"\twc_knocking\sounds\knockMetal.ogg", 2, 1};
 		titles[] = {};
