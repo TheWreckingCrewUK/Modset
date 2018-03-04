4c4c65fdd0e6adead38d083695dd5d6b4cda0d9f
Justin Katulski <jkatulski96@gmail.com>
2018-03-04 11:42:22 -0500

    twc non scopping L7

---
+++ @twc_framework/addons/twc_gearFixes/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches {
+	class TWC_gearFixes {
+		units[]={};
+		weapons[]={};
+		requiredVersion = 1.7;
+		requiredAddons[] = {};
+		author[]={};
+		authorUrl="";
+		version="1";
+		versionStr="1";
+		versionAr[]={1};
+	};
+};
+class cfgWeapons{
+	class UK3CB_BAF_L7A2;
+	class twc_L7A2: UK3CB_BAF_L7A2{
+		displayname = "TWC Edited L7A2";
+		class WeaponSlotsInfo{
+			class asdg_OpticRail_UK3CB_BAF_L7A2{
+				
+			};
+		};
+	};
+};
