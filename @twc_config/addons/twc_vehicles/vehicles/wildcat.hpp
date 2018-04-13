f72631ed438709e79102d4a39a3ecaf6f02e3aef
Bosenator <strooptroopers+ghbose@gmail.com>
2018-04-13 22:03:46 +0100

    change to 8 sitter

--- @twc_config/addons/twc_vehicles/vehicles/wildcat.hpp
+++ @twc_config/addons/twc_vehicles/vehicles/wildcat.hpp
@@ -1,10 +1,12 @@
-class UK3CB_BAF_Wildcat_AH1_CAS_6A;
+class UK3CB_BAF_Wildcat_AH1_CAS_8A;
 
-class TWC_BaseClass_Wildcat: UK3CB_BAF_Wildcat_AH1_CAS_6A {
+class TWC_BaseClass_Wildcat: UK3CB_BAF_Wildcat_AH1_CAS_8A {
+	scope = 1;
+
 	class AnimationSources;
 };
 
-class TWC_BaseClass_Wildcat_F: UK3CB_BAF_Wildcat_AH1_CAS_6A {
+class TWC_BaseClass_Wildcat_F: UK3CB_BAF_Wildcat_AH1_CAS_8A {
 	class AnimationSources {
 		class ClanLogo_Hide {
 			animPeriod = 1e-006;
