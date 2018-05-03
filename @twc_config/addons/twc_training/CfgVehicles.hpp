623d80f2803367c61571e83198f8488bfadd43a7
Bosenator <strooptroopers+ghbose@gmail.com>
2018-05-03 22:48:57 +0100

    Units & Backpacks for Trainer/Medic Trainer

--- @twc_config/addons/twc_training/CfgVehicles.hpp
+++ @twc_config/addons/twc_training/CfgVehicles.hpp
@@ -1,4 +1,108 @@
 class CfgVehicles {
+	class UK3CB_BAF_B_Bergen_MTP_SL_L_A;
+	class UK3CB_BAF_B_Bergen_MTP_Medic_L_A;
+	
+	class TWC_Backpack_Trainer: UK3CB_BAF_B_Bergen_MTP_SL_L_A {
+		scope = 1;
+		mass = 59.84;
+		
+		class TransportItems {
+			class _xx_PRC_117F {
+				name = "ACRE_PRC117F";
+				count = 1;
+			};
+		};
+		
+		class TransportMagazines {
+			class _xx_Smoke {
+				magazine = "SmokeShell";
+				count = 2;
+			};
+
+			class _xx_Smoke_Red {
+				magazine = "SmokeShellRed";
+				count = 3;
+			};
+
+			class _xx_Smoke_Green {
+				magazine = "SmokeShellGreen";
+				count = 3;
+			};
+
+			class _xx_Smoke_Blue {
+				magazine = "SmokeShellBlue";
+				count = 3;
+			};
+		};
+	};
+	
+	class TWC_Backpack_Trainer_Medical: UK3CB_BAF_B_Bergen_MTP_Medic_L_A {
+		scope = 1;
+		class TransportItems {
+			class _xx_Bandage {
+				name = "ACE_fieldDressing";
+				count = 12;
+			};
+			
+			class _xx_Morphine {
+				name = "ACE_morphine";
+				count = 10;
+			};
+			
+			class _xx_ACE_elasticBandage {
+				name = "ACE_elasticBandage";
+				count = 15;
+			};
+			
+			class _xxACE_quikclot {
+				name = "ACE_quikclot";
+				count = 6;
+			};
+			
+			class _xxACE_adenosine {
+				name = "ACE_adenosine";
+				count = 4;
+			};
+			
+			class _xxACE_atropine {
+				name = "ACE_atropine";
+				count = 4;
+			};
+			
+			class _xxACE_epinephrine {
+				name = "ACE_epinephrine";
+				count = 10;
+			};
+			
+			class _xxACE_packingBandage {
+				name = "ACE_packingBandage";
+				count = 15;
+			};
+			
+			class _xxACE_personalAidKit {
+				name = "ACE_personalAidKit";
+				count = 1;
+			};
+			
+			class _xxACE_salineIV_500 {
+				name = "ACE_salineIV_500";
+				count = 6;
+			};
+			
+			class _xxACE_tourniquet {
+				name = "ACE_tourniquet";
+				count = 4;
+			};
+		};
+		
+		class TransportMagazines {
+			class _xx_Smoke {
+				magazine = "SmokeShell";
+				count = 3;
+			};
+		};
+	};
+	
 	class B_Soldier_base_F;
 	
 	class TWC_Units_Training_Trainer: B_Soldier_base_F {
@@ -10,6 +114,7 @@ class CfgVehicles {
 		icon = "iconManLeader":
 		nakedUniform = "U_BasicBody";
 		uniformClass = "UK3CB_BAF_U_CombatUniform_MTP";
+		backpack = "TWC_Backpack_Trainer";
 		
 		class EventHandlers: EventHandlers {
 			init = "(_this select 0) setVariable [""twc_keepMap"", true]";
@@ -127,6 +232,47 @@ class CfgVehicles {
 			"UK3CB_BAF_556_30Rnd_T",
 			"CUP_HandGrenade_L109A2_HE",
 			"CUP_HandGrenade_L109A2_HE",
+			"SmokeShell",
+			"SmokeShell",
+			"SmokeShell"
+		};
+	};
+	
+	class TWC_Units_Training_Medical_Trainer: TWC_Units_Training_Trainer {
+		displayName = "Trainer (Medical)";
+		icon = "iconManMedic";
+		backpack = "TWC_Backpack_Trainer_Medical";
+		attendant = 1;
+		
+		weapons[] = {
+			"Throw",
+			"Put",
+			"UK3CB_BAF_L85A2"
+		};
+
+		respawnweapons[] = {
+			"Throw",
+			"Put",
+			"UK3CB_BAF_L85A2"
+		};
+
+		magazines[] = {
+			"UK3CB_BAF_556_30Rnd",
+			"UK3CB_BAF_556_30Rnd",
+			"UK3CB_BAF_556_30Rnd",
+			"SmokeShell",
+			"SmokeShell",
+			"SmokeShell",
+			"SmokeShell",
+			"SmokeShell"
+		};
+
+		respawnmagazines[] = {
+			"UK3CB_BAF_556_30Rnd",
+			"UK3CB_BAF_556_30Rnd",
+			"UK3CB_BAF_556_30Rnd",
+			"SmokeShell",
+			"SmokeShell",
 			"SmokeShell",
 			"SmokeShell",
 			"SmokeShell"
