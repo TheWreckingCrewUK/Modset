8f0240acd374179972e20395063e1626460a551e
Bosenator <strooptroopers+ghbose@gmail.com>
2018-05-22 11:26:36 +0100

    bring inline with twc tan counterparts

--- @twc_config_ww2/addons/twc_ww2_faction/backpacks.hpp
+++ @twc_config_ww2/addons/twc_ww2_faction/backpacks.hpp
@@ -1,4 +1,4 @@
 class fow_b_uk_p37;
 class fow_b_uk_p37_blanco: fow_b_uk_p37 {
-	maximumLoad = 100;
+	maximumLoad = 200;
 };
--- @twc_config_ww2/addons/twc_ww2_faction/vests.hpp
+++ @twc_config_ww2/addons/twc_ww2_faction/vests.hpp
@@ -5,27 +5,27 @@ class fow_v_base;
 class fow_v_uk_base: fow_v_base { class ItemInfo; };
 class fow_v_uk_base_green: fow_v_uk_base {
 	class ItemInfo: ItemInfo {
-		containerClass = "Supply90";
+		containerClass = Supply160;
 	};
 };
 
 class fow_v_uk_bren: fow_v_base { class ItemInfo; };
 class fow_v_uk_bren_green: fow_v_uk_bren {
 	class ItemInfo: ItemInfo {
-		containerClass = "Supply150";
+		containerClass = Supply160;
 	};
 };
 
 class fow_v_uk_para_sten: fow_v_base { class ItemInfo; };
 class fow_v_uk_para_sten_green: fow_v_uk_para_sten {
 	class ItemInfo: ItemInfo {
-		containerClass = "Supply100";
+		containerClass = Supply140;
 	};
 };
 
 class fow_v_uk_officer: fow_v_base { class ItemInfo; };
 class fow_v_uk_officer_green: fow_v_uk_officer {
 	class ItemInfo: ItemInfo {
-		containerClass = "Supply80";
+		containerClass = Supply140;
 	};
 };
