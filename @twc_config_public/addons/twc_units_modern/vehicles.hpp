c8231e3e0e688456ac9149a79feaafd6b173c88c
Bosenator <Bosenator@users.noreply.github.com>
2018-06-30 16:52:55 +0100

    fix for hobbs

--- @twc_config_public/addons/twc_units_modern/vehicles.hpp
+++ @twc_config_public/addons/twc_units_modern/vehicles.hpp
@@ -127,23 +127,21 @@ class CUP_Wolfhound_Base : Wheeled_APC_F
 	
 	
 	class UK3CB_BAF_LandRover_Hard_FFR_Green_A;
-    class UK3CB_BAF_LandRover_Hard_FFR_Green_A_TWCBaseRadio: UK3CB_BAF_LandRover_Hard_FFR_Green_A {
+	class UK3CB_BAF_LandRover_Hard_FFR_Green_A_TWCBaseRadio: UK3CB_BAF_LandRover_Hard_FFR_Green_A {
 		displayname = "Land Rover TWC Base Radio";
-        class AcreRacks {
-           class Rack_1 {
-               displayName = "Dash"; // Name is displayed in the interaction menu.
-               shortName = "Dash";
-               componentName = "ACRE_VRC103";
-               allowedPositions[] = {"inside", "external"}; // Who has access. "inside" - anyone inside, "external" - provides access upto 10m away, "driver", "gunner", "copilot", "commander"
-               disabledPositions[] = {};
-               defaultComponents[] = {};
-               mountedRadio = "ACRE_PRC117F";                 // Predefined mounted radio
-               isRadioRemovable = 0;
-    
-           };
-       }
-    };
-	
+		class AcreRacks {
+	   		class Rack_1 {
+		       		displayName = "Dash"; // Name is displayed in the interaction menu.
+				shortName = "Dash";
+				componentName = "ACRE_VRC103";
+				allowedPositions[] = {"inside", "external"}; // Who has access. "inside" - anyone inside, "external" - provides access upto 10m away, "driver", "gunner", "copilot", "commander"
+				disabledPositions[] = {};
+				defaultComponents[] = {};
+				mountedRadio = "ACRE_PRC117F"; // Predefined mounted radio
+				isRadioRemovable = 0;
+			};
+       		};
+	};	
 	
 	
 	/*
