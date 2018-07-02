cca3dd2bbc29ea610f69d3e08dfdafcb1126ab63
Bosenator <strooptroopers+ghbose@gmail.com>
2018-07-03 00:28:31 +0100

    C130J troop & viv - radio & intercom

--- @twc_config/addons/twc_vehicles/coldwar/coldwar_plane.hpp
+++ @twc_config/addons/twc_vehicles/coldwar/coldwar_plane.hpp
@@ -14,4 +14,41 @@ class TWC_Vehicle_ColdWar_HarrierGR3_RAF: CUP_B_GR9_DYN_GB {
 	class TransportMagazines { };
 	class TransportWeapons { };
 	class TransportBackpacks { };
+	
+	class AcreRacks {
+		class Rack_1 {
+			displayName = "Vehicle Radio";
+			shortName = "Vehicle Radio";
+			componentName = "ACRE_VRC64";
+			allowedPositions[] = {"crew"};
+			disabledPositions[] = {};
+			defaultComponents[] = {};
+			mountedRadio = "ACRE_PRC77";
+			isRadioRemovable = 0;
+			intercom[] = {"Intercom_1"};
+		};
+	};
+};
+
+class TWC_Vehicle_ColdWar_Hercules_RAF_VIV: TWC_Vehicle_Generic_Hercules_RAF_VIV {
+	scope = 2;
+	side = 1;
+	ADD_FACTION;
+
+	displayname = "Hercules (Cold War) - VIV (RAF)";
+	editorSubcategory = "TWC_ForceType_Airborne";
+
+	class AcreRacks {
+		class Rack_1 {
+			displayName = "Vehicle Radio";
+			shortName = "Vehicle Radio";
+			componentName = "ACRE_VRC64";
+			allowedPositions[] = {"crew"};
+			disabledPositions[] = {};
+			defaultComponents[] = {};
+			mountedRadio = "ACRE_PRC77";
+			isRadioRemovable = 0;
+			intercom[] = {"Intercom_2"};
+		};
+	};
 };
