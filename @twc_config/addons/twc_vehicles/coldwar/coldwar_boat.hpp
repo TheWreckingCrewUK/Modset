016d7ac341f34a45b331b6ed66ac11563ab87139
Bosenator <strooptroopers+ghbose@gmail.com>
2018-07-03 01:01:33 +0100

    Zodiac Radio

--- @twc_config/addons/twc_vehicles/coldwar/coldwar_boat.hpp
+++ @twc_config/addons/twc_vehicles/coldwar/coldwar_boat.hpp
@@ -1,5 +1,5 @@
 #undef CURRENT_VEHICLE
-#define CURRENT_VEHICLE GENERIC_EMPTY
+#define CURRENT_VEHICLE COLDWAR_BOAT
 class I_C_Boat_Transport_02_F;
 class TWC_Vehicle_Generic_RHIB_Black: I_C_Boat_Transport_02_F {
 	scope = 2;
@@ -29,22 +29,4 @@ class TWC_Vehicle_Generic_RHIB_Black: I_C_Boat_Transport_02_F {
 			isRadioRemovable = 0;
 		};
 	};
-};
-
-class I_C_Boat_Transport_01_F;
-class TWC_Vehicle_Generic_Zodiac_Black: I_C_Boat_Transport_01_F {
-	scope = 2;
-	scopeCurator = 2;
-	side = 1;
-	ADD_FACTION;
-	
-	displayname = "Zodiac (Black)";
-	editorSubcategory = "TWC_ForceType_Amphibious";
-
-	class TransportItems { }; 
-	class TransportMagazines { };
-	class TransportWeapons { };
-	class TransportBackpacks { };
-
-	ace_interaction_canPush = 1;
 };
