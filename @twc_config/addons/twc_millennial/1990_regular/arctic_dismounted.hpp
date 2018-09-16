70ee5fb4570d63dcfc1d1694997b48cbf0fe2305
Frozeph <31446709+Frozeph@users.noreply.github.com>
2018-09-16 11:21:59 +0100

    For Checking by Riko

---
+++ @twc_config/addons/twc_millennial/1990_regular/arctic_dismounted.hpp
@@ -0,0 +1,49 @@
+//REGULAR, DISMOUNTED, Arctic
+//Section
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Rifleman: TWC_Infantry_1990_Regular_Arctic_Rifleman
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Rifleman";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_AT: TWC_Infantry_1990_Regular_Arctic_AT
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_AT";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Gunner: TWC_Infantry_1990_Regular_Arctic_Gunner
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Gunner";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_SectionCommander: TWC_Infantry_1990_Regular_Arctic_SectionCommander
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_SectionCommander";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_2iC: TWC_Infantry_1990_Regular_Arctic_2iC
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_2iC";
+};
+
+//Platoon Command
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Platoon_Commander: TWC_Infantry_1990_Regular_Arctic_Platoon_Commander
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Platoon_Commander";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Platoon_Sergeant: TWC_Infantry_1990_Regular_Arctic_Platoon_Sergeant
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Platoon_Sergeant";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Platoon_Medic: TWC_Infantry_1990_Regular_Arctic_Platoon_Medic
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Platoon_Medic";
+};
+class TWC_Infantry_1990_Regular_Dismounted_Arctic_Platoon_CSM: TWC_Infantry_1990_Regular_Arctic_Platoon_CSM
+{
+	CATEGORY(TWC_Infantry_Dismounted_Arctic)
+	backpack = "TWC_Backpack_1990_Regular_Dismounted_Desert_Platoon_Sergeant";
+};
