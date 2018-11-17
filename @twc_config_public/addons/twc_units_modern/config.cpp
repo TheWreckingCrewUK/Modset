8cfb51fd33873a07f971c9884505f369b55e81b7
Hobbnob <thehobbnob@gmail.com>
2018-11-17 00:48:19 +0000

    polish AT buff WIP
    making a version of the RPG76 from the Pzf60 for light AT use,
    potentially adding milan to them under the name of spike if all goes
    well

--- @twc_config_public/addons/twc_units_modern/config.cpp
+++ @twc_config_public/addons/twc_units_modern/config.cpp
@@ -66,6 +66,7 @@ class CfgPatches {
 			"a3_characters_f_gamma",
 			"rhsusf_vehicles",
 			"rhs_c_weapons",
+			"rhsgref_c_weapons",
 			"rhsusf_c_weapons",
 			"uk3cb_baf_vehicles_landrover",
 			"uk3cb_baf_weapons_smallarms"
