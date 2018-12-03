60e8f890ba6c572e980c67bbebc4f859a5992591
Neciota <neciota@gmail.com>
2018-12-04 00:11:13 +0100

    Made Chinook STRONGER, FASTER, BETTER
    So that it can carry muh CVRT again.

--- @twc_config_compatibility/addons/twc_gearFixes/config.cpp
+++ @twc_config_compatibility/addons/twc_gearFixes/config.cpp
@@ -753,17 +753,8 @@ class CfgVehicles {
 		};
 	};
 	class Helicopter_Base_H;
-	class CUP_CH47F_base: Helicopter_Base_H
-	{
-		backRotorForceCoef = 0.9;
-		cyclicForwardForceCoef = 1.5;
-		cyclicAsideForceCoef = 1.3;
-		liftForceCoef = 0.9;
-		bodyFrictionCoef = 1.4;
-		armorStructural = 20;
-	};
-		
-		
+	class CUP_CH47F_base;
+
 //handling modifications
 	class Car;
 	class Car_F: Car{
