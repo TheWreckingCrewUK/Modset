06887c2ad1f7a6e875a7835cd37b6d3b2a570e1c
Hobbnob <thehobbnob@gmail.com>
2018-12-04 16:11:38 +0000

    bad rik
    tells him he's allowed to revert the engine power reduction > deletes
    everything instead

--- @twc_config_compatibility/addons/twc_gearFixes/config.cpp
+++ @twc_config_compatibility/addons/twc_gearFixes/config.cpp
@@ -762,7 +762,14 @@ class CfgVehicles {
 		};
 	};
 	class Helicopter_Base_H;
-	class CUP_CH47F_base;
+	class CUP_CH47F_base: Helicopter_Base_H
+	{
+		backRotorForceCoef = 0.9;
+		cyclicForwardForceCoef = 1.5;
+		cyclicAsideForceCoef = 1.3;
+		bodyFrictionCoef = 1.4;
+		armorStructural = 20;
+	};
 
 //handling modifications
 	class Car;
