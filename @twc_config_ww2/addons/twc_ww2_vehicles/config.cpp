cdbd53fb7bd7cd16cd0093090bb9eccd0ee90c4e
Bosenator <strooptroopers+ghbose@gmail.com>
2018-12-29 17:22:51 +0000

    Christmas OP patches
    Fixes from the Christmas Branch; Wire Cutter support on a CSA38 fence,
    harmless support for CSA MG38 & better feedback for the dummyBullets
    function.

--- @twc_config_ww2/addons/twc_ww2_vehicles/config.cpp
+++ @twc_config_ww2/addons/twc_ww2_vehicles/config.cpp
@@ -148,4 +148,10 @@ class CfgVehicles
 	{
 		faction = "twc_faction_independent";
 	};
+	
+	/** WIRECUTTER SUPPORT **/
+	class Land_WW2_Wire_1;
+	class Land_WW2_Wire_Bruno: Land_WW2_Wire_1 {
+		ace_logistics_wirecutter_isFence = 1;
+	};
 };
