c8ed9875eb93b3736f499d6d71f52bc1ce0e3ccf
Neciota <neciota@gmail.com>
2019-01-07 14:44:34 +0100

    Argentinian Units
    Falklands War, Argie Units.

--- @twc_config/addons/twc_opfor_Misc/vehicles/backpacks.hpp
+++ @twc_config/addons/twc_opfor_Misc/vehicles/backpacks.hpp
@@ -1,6 +1,8 @@
 class CUP_B_AlicePack_Khaki;
 class CUP_B_RPGPack_Khaki;
 class CUP_B_HikingPack_Civ;
+class CUP_B_AlicePack_OD;
+class SP_Backpack_BlowpipeCarrier;
 
 class Afr_Backpack_Rifleman: CUP_B_AlicePack_Khaki {
 	class TransportMagazines {
@@ -49,5 +51,27 @@ class WaPAT_Backpack_AT: CUP_B_HikingPack_Civ {
 
 	class TransportMagazines {
 		maga_nc(CUP_AT13_M, 2);
+	};
+};
+
+class TWC_Backpack_Argentina_1980_SectionCommander: CUP_B_AlicePack_OD
+{
+	scope = 1;
+	scopeArsenal = 1;
+	class TransportItems {
+		item_nc(ACRE_PRC117F,1);
+	};
+};
+class TWC_Backpack_Argentina_1980_2iC: CUP_B_AlicePack_OD
+{
+	class TransportMagazines {
+		maga_nc(CUP_20Rnd_762x51_FNFAL_M, 6);
+		maga_nc(UK3CB_BAF_762_100Rnd_T, 1);
+	};
+};
+class TWC_Backpack_Argentina_1980_AA: SP_Backpack_BlowpipeCarrier
+{
+	class TransportMagazines {
+		maga_nc(SP_Blowpipe_round, 1);
 	};
 };
