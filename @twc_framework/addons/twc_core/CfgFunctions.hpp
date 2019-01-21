be502d72b04d9a387bcefd0b8f115e91eff523d9
Bosenator <strooptroopers+ghbose@gmail.com>
2019-01-21 16:36:34 +0000

    +drawBoundingBox

--- @twc_framework/addons/twc_core/CfgFunctions.hpp
+++ @twc_framework/addons/twc_core/CfgFunctions.hpp
@@ -26,6 +26,9 @@ class CfgFunctions {
 			/** UTILITY FUNCTIONS **/
 			class findGreatestDistanceWithArray {};
 			class findGreatestDistancePairInArray {};
+			
+			/** DEBUG TOOLS **/
+			class drawBoundingBox {};
 		};
 	};
 };
