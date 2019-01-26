50947d811dda17f11bf8e131bcabbdb87110c68c
Bosenator <strooptroopers+ghbose@gmail.com>
2019-01-26 07:00:04 +0000

    Enable Client addToGroup notification

--- @twc_framework/addons/twc_core/CfgEventHandlers.hpp
+++ @twc_framework/addons/twc_core/CfgEventHandlers.hpp
@@ -1,6 +1,6 @@
 class Extended_InitPost_EventHandlers {
 	class twc_core_eh {
 		serverInit = "[] call TWC_Core_fnc_serverEventHandlers;";
-		//clientInit = "[] call TWC_Core_fnc_clientEventHandlers;";
+		clientInit = "[] call TWC_Core_fnc_clientEventHandlers;";
 	};
 };
