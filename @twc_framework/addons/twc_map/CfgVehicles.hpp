1c6ea3e3aff155e66cd27b4252e7c1c9a41cdc24
Bosenator <strooptroopers+ghbose@gmail.com>
2019-02-10 18:20:33 +0000

    classname case matters

--- @twc_framework/addons/twc_map/CfgVehicles.hpp
+++ @twc_framework/addons/twc_map/CfgVehicles.hpp
@@ -68,8 +68,8 @@ class CfgVehicles {
 	// third party maps
 	CREATE_MAP(Carraigdubh);
 	CREATE_MAP(utes);
-	CREATE_MAP(Chernarus);
-	CREATE_MAP(Chernarus_Summer);
-	CREATE_MAP(Chernarus_Winter);
+	CREATE_MAP(chernarus);
+	CREATE_MAP(chernarus_summer);
+	CREATE_MAP(chernarus_winter);
 	CREATE_MAP(pja307); // Dariyah
 };
