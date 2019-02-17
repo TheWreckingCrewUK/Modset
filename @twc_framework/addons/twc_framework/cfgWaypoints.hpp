66bf501036778e24d26c0390d6fe59abdd59f568
Justin Katulski <jkatulski96@gmail.com>
2019-02-17 14:10:22 -0500

    Added Move Timid Waypoint
    It can be used to give the impression of being followed as well as a way to stop civillian cars from blowing through our platoon at 100 mph

--- @twc_framework/addons/twc_framework/cfgWaypoints.hpp
+++ @twc_framework/addons/twc_framework/cfgWaypoints.hpp
@@ -16,5 +16,12 @@ class cfgWaypoints {
 			file = "twc_framework\waypoints\fn_wpMoveAndDelete.sqf";
 			icon = "\a3\3den\Data\CfgWaypoints\getInNearest_ca.paa";
 		};
+		
+		class wpMoveTimid {
+			displayName = "Move Timid";
+			displayNameDebug = "wpMoveTimid";
+			file = "twc_framework\waypoints\fn_wpMoveTimid.sqf";
+			icon = "\a3\3den\Data\CfgWaypoints\getInNearest_ca.paa";
+		};
 	};
 };
