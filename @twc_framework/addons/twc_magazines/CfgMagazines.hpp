650b235e8003b29fac333ea75c4c7fdcfc7a45ed
Bosenator <strooptroopers+ghbose@gmail.com>
2019-02-21 22:10:27 +0000

    New attempt with new idea

---
+++ @twc_framework/addons/twc_magazines/CfgMagazines.hpp
@@ -0,0 +1,51 @@
+class CfgMagazines {
+	class CA_Magazine;
+	class TWC_Loose_Ammo_Base_Class: CA_Magazine {};
+
+	// DEBUG: When finished, move to the repos respective config/weapons folder!
+	class UK3CB_BAF_556_30Rnd: CA_Magazine {
+		deleteIfEmpty = 0;
+	};
+
+	class TWC_Magazine_LooseAmmo_556: TWC_Loose_Ammo_Base_Class {
+		scope = 2;
+		scopeCurator = 1;
+
+		displayName = "Loose 5.56 Ammo";
+		displayNameShort = "Loose 5.56 Ammo";
+		descriptionShort = "Loose 5.56 Ammo";
+		picture = ""; // path
+		count = 300;
+		weight = 1;
+
+		TWC_Fills[] = {"UK3CB_BAF_556_30Rnd"}; // classnames of the magazines this fills
+	};
+
+	class TWC_Magazine_LooseAmmo_556_T: TWC_Loose_Ammo_Base_Class {
+		scope = 2;
+		scopeCurator = 1;
+
+		displayName = "Loose 5.56 Tracer Ammo";
+		displayNameShort = "Loose 5.56 Tracer Ammo";
+		descriptionShort = "Loose 5.56 Tracer Ammo";
+		picture = ""; // path
+		count = 300;
+		weight = 1;
+
+		TWC_Fills[] = {"UK3CB_BAF_556_30Rnd_T"}; // classnames of the magazines this refills
+	};
+
+	class TWC_Magazine_LooseAmmo_303: TWC_Loose_Ammo_Base_Class {
+		scope = 2;
+		scopeCurator = 1;
+
+		displayName = "Loose .303 Ammo";
+		displayNameShort = "Loose .303 Ammo";
+		descriptionShort = "Loose .303 Ammo";
+		picture = ""; // path
+		count = 100;
+		weight = 1;
+
+		TWC_Fills[] = {""}; // classnames of the magazines this refills
+	};
+};
---
+++ @twc_framework/addons/twc_magazines/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches {
+	class twc_magazines {
+		units[]={};
+		weapons[]={};
+		requiredVersion = 1.7;
+
+		requiredAddons[] = {
+			"TWC_Core",
+			"ace_common",
+			"ace_interaction",
+			"ace_magazinerepack"
+		};
+
+		author[]={};
+		authorUrl="";
+		version="1";
+		versionStr="1";
+		versionAr[]={1};
+	};
+};
+
+#include "CfgFunctions.hpp"
+#include "CfgMagazines.hpp"
