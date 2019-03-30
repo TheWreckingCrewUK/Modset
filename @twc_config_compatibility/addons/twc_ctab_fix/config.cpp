b481edbdc6b393965560475d3ac18614f576d2f9
Bosenator <strooptroopers+ghbose@gmail.com>
2019-03-30 10:59:12 +0000

    add cTab_Tablet_RscMapControl

--- @twc_config_compatibility/addons/twc_ctab_fix/config.cpp
+++ @twc_config_compatibility/addons/twc_ctab_fix/config.cpp
@@ -1,4 +1,4 @@
-class CfgPatches
+class CfgPatches {
 	class TWC_cTab_fixes {
 		units[] = {};
 		weapons[] = {};
@@ -11,9 +11,10 @@ class CfgPatches
 	};
 };
 
+class cTab_RscMapControl;
 class RscTitles {
 	titles[] = {};
-	
+
 	class cTab_mapSize_dsp {
 		idd = 13673;
 		onLoad = "uiNamespace setVariable ['cTab_mapSize_dsp', _this select 0]";
@@ -48,3 +49,25 @@ class RscTitles {
 		};
 	};
 };
+
+class cTab_Tablet_RscMapControl: cTab_RscMapControl {
+	text = "#(argb,8,8,3)color(1,1,1,1)";
+	x = "(((257))) / 2048  *  ( (safezoneH * 1.2) * 3/4) +  (safezoneX + (safezoneW -  ( (safezoneH * 1.2) * 3/4)) / 2 + ( ( (safezoneH * 1.2) * 3/4) * 96.5 / 2048))";
+	y = "(((491) + (42))) / 2048  *  (safezoneH * 1.2) +  (safezoneY + (safezoneH -  (safezoneH * 1.2)) / 2)";
+	w = "(((1341))) / 2048  *  ( (safezoneH * 1.2) * 3/4)";
+	h = "(((993) - (42) - (0))) / 2048  *  (safezoneH * 1.2)";
+	maxSatelliteAlpha = 10000;
+	alphaFadeStartScale = 10;
+	alphaFadeEndScale = 10;
+	ptsPerSquareSea = "8 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareTxt = "8 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareCLn = "8 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareExp = "8 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareCost = "8 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareFor = "3 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareForEdge = "100 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareRoad = "1.5 / (0.86 /  (safezoneH * 1.2))";
+	ptsPerSquareObj = "4 / (0.86 /  (safezoneH * 1.2))";
+	widthRailWay = 1;
+};
+
