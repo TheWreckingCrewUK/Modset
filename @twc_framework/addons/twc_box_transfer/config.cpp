e6f95b5f7e85a5d5db00fa4a45fb9f2775ac4a62
Bosenator <strooptroopers+ghbose@gmail.com>
2019-06-24 00:05:39 +0100

    CfgPatches box transfer

---
+++ @twc_framework/addons/twc_box_transfer/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches {
+	class TWC_Box_Transfers {
+		units[]={};
+		weapons[]={};
+		requiredVersion = 1.7;
+
+		requiredAddons[] = {
+			"ace_common"
+		};
+
+		author[] = {};
+		authorUrl = "";
+		version = "1";
+		versionStr = "1";
+		versionAr[] = {1};
+	};
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgVehicles.hpp"
