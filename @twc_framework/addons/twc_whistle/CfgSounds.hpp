ff51d856e6cb6ba8f53aec90588b1832f900a2bb
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-06 13:42:27 +0100

    add whistle sound

---
+++ @twc_framework/addons/twc_whistle/CfgSounds.hpp
@@ -0,0 +1,21 @@
+class CfgSounds {
+	sounds[] = {"TWC_Whistle_Start", "TWC_Whistle_Stop", "TWC_Whistle_Loop"};
+	
+	class TWC_Whistle_Start {
+		name = "TWC_Whistle_Start";
+		sound[] = {"sounds\whistle_start.ogg", 1, 1, 250};
+		titles[] = {};
+	};
+	
+	class TWC_Whistle_Stop {
+		name = "TWC_Whistle_Stop";
+		sound[] = {"sounds\whistle_stop.ogg", 1, 1, 250};
+		titles[] = {};
+	};
+	
+	class TWC_Whistle_Loop {
+		name = "TWC_Whistle_Loop";
+		sound[] = {"sounds\whistle_loop.ogg", 1, 1, 250};
+		titles[] = {};
+	};
+};
