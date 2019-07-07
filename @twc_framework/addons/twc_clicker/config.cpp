5edd0bf0455d4bbc07b877c5ed8b5470dc9b1b8e
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-07 18:48:19 +0100

    clicker code, needs actual clicker sound however

---
+++ @twc_framework/addons/twc_clicker/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches {
+	class TWC_Clicker {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.7;
+		
+		requiredAddons[] = {
+			"cba_main"
+		};
+		
+		author = "Bosenator";
+		authorUrl = "thewreckingcrew.eu";
+		version = "1";
+		versionStr = "1";
+		versionAr[] = {1};
+	};
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgSounds.hpp"
+#include "CfgWeapons.hpp"
