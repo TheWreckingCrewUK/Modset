beb3749e31d999f09bd64725350995b16b3809eb
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-08 01:49:05 +0100

    landvehicle disabled box transfer
    Commented out, trying to figure out build issue

--- @twc_framework/addons/twc_box_transfer/CfgVehicles.hpp
+++ @twc_framework/addons/twc_box_transfer/CfgVehicles.hpp
@@ -14,18 +14,18 @@ class CfgVehicles {
 		};
 	};
 	
-	class LandVehicle;
-	class Car: LandVehicle {
-		class ACE_Actions {
-			class ACE_MainActions {
-				class TWC_Transfer_Contents {
-					displayName = "Transfer Contents";
-					condition = "true";
-					exceptions[] = {"isNotInside", "isNotSwimming", "isNotSitting"};
-					insertChildren = "_this call TWC_Box_Transfer_fnc_getTransferChildren";
-					icon = "twc_box_transfer\ui\transfer_ca.paa";
-				};
-			};
-		};
-	};
+	// class LandVehicle;
+	// class Car: LandVehicle {
+		// class ACE_Actions {
+			// class ACE_MainActions {
+				// class TWC_Transfer_Contents {
+					// displayName = "Transfer Contents";
+					// condition = "true";
+					// exceptions[] = {"isNotInside", "isNotSwimming", "isNotSitting"};
+					// insertChildren = "_this call TWC_Box_Transfer_fnc_getTransferChildren";
+					// icon = "twc_box_transfer\ui\transfer_ca.paa";
+				// };
+			// };
+		// };
+	// };
 };
