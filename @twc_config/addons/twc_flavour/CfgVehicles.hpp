37536b36aa1e32a55c5ce7c0c8eeb3b0d25b901d
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-09 16:31:34 +0100

    Clean up TWC_Flavour
    In prep for better commonality

--- @twc_config/addons/twc_flavour/CfgVehicles.hpp
+++ @twc_config/addons/twc_flavour/CfgVehicles.hpp
@@ -1,8 +1,7 @@
-class CfgVehicles
-{
+class CfgVehicles {
 	class Sign_Checkpoint_US_EP1;
-	class TWC_Objects_Checkpoint_UK: Sign_Checkpoint_US_EP1
-	{
+	
+	class TWC_Objects_Checkpoint_UK: Sign_Checkpoint_US_EP1 {
 		displayName = "Checkpoint (UK)";
 		hiddenSelectionsTextures[] = {"\twc_flavour\data\checkpoint_sign_co.paa"};
 	};
