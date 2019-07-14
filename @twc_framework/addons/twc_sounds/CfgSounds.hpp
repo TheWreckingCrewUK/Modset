c957a7d19701d1833bbebc08526ab46e7783a4fc
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-14 14:33:54 +0100

    Relocating sounds

--- @twc_framework/addons/twc_sounds/CfgSounds.hpp
+++ @twc_framework/addons/twc_sounds/CfgSounds.hpp
@@ -19,4 +19,23 @@ class CfgSounds {
 	ADD_SOUND(drowned,death);
 	ADD_SOUND(instant_death,death);
 	ADD_SOUND(overdose,death);
+	
+	/** GAME SOUNDS **/
+	ADD_SOUND(banzai_1,ww2);
+	ADD_SOUND(banzai_2,ww2);
+	ADD_SOUND(banzai_3,ww2);
+	ADD_SOUND(banzai_4,ww2);
+	ADD_SOUND(banzai_5,ww2);
+	
+	ADD_SOUND(radio_chatter,radio);
+	ADD_SOUND(radio_dunkirk_german,radio);
+	ADD_SOUND(radio_dunkirk_us,radio);
+	ADD_SOUND(radio_norsk,radio);
+	
+	ADD_SOUND(bugle_call,other);
+	
+	ADD_SOUND(arab_crowd,crowds);
+	
+	ADD_SOUND(airraid_warning,alarms);
+	ADD_SOUND(airraid_allclear,alarms);
 };
