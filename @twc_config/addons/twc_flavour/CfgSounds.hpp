58f7349f433d7cabdf9152330fcc3f68a60ff09f
Bosenator <strooptroopers+ghbose@gmail.com>
2019-07-14 13:14:06 +0100

    macrotise cfgsounds for twc_flavour

--- @twc_config/addons/twc_flavour/CfgSounds.hpp
+++ @twc_config/addons/twc_flavour/CfgSounds.hpp
@@ -1,65 +1,30 @@
+#define ADD_SOUND(CLASSNAME,FOLDER) \
+	class TWC_##CLASSNAME { \
+		name = "TWC_##CLASSNAME"; \
+		sound[] = { \
+			twc_flavour\sounds\##FOLDER##\##CLASSNAME##.ogg \
+			, 1.0, 1.0 }; \
+		titles[] = {}; \
+	}
+
 class CfgSounds {
 	sounds[] = {};
 	
-	class twc_banzai_1 {
-		name = "banzai_1";
-		sound[] = {"twc_flavour\sounds\ww2\banzai_1.ogg", 1, 1};
-		titles[] = {};
-	};
+	ADD_SOUND(banzai_1,ww2);
+	ADD_SOUND(banzai_2,ww2);
+	ADD_SOUND(banzai_3,ww2);
+	ADD_SOUND(banzai_4,ww2);
+	ADD_SOUND(banzai_5,ww2);
 	
-	class twc_banzai_2: twc_banzai_1 {
-		name = "banzai_2";
-		sound[] = {"twc_flavour\sounds\ww2\banzai_2.ogg", 1, 1};
-	};
-
-	class twc_banzai_3: twc_banzai_1 {
-		name = "banzai_2";
-		sound[] = {"twc_flavour\sounds\ww2\banzai_2.ogg", 1, 1};
-	};
+	ADD_SOUND(radio_chatter,radio);
+	ADD_SOUND(radio_dunkirk_german,radio);
+	ADD_SOUND(radio_dunkirk_us,radio);
+	ADD_SOUND(radio_norsk,radio);
 	
-	class twc_banzai_4: twc_banzai_1 {
-		name = "banzai_2";
-		sound[] = {"twc_flavour\sounds\ww2\banzai_2.ogg", 1, 1};
-	};
+	ADD_SOUND(bugle_call,other);
 	
-	class twc_banzai_5: twc_banzai_1 {
-		name = "banzai_2";
-		sound[] = {"twc_flavour\sounds\ww2\banzai_2.ogg", 1, 1};
-	};
+	ADD_SOUND(arab_crowd,crowds);
 	
-	class twc_radio_chatter {
-		name = "radio_chatter";
-		sound[] = {"twc_flavour\sounds\radio\radio_chatter.ogg", 1, 1};
-		titles[] = {};
-	};
-	
-	class twc_bugle_call {
-		name = "bugle_call";
-		sound[] = {"twc_flavour\sounds\other\bugle_call.ogg", 1, 1};
-		titles[] = {};
-	};
-	
-	class twc_arab_crowd {
-		name = "arab_crowd";
-		sound[] = {"twc_flavour\sounds\crowds\arab_crowd.ogg", 1, 1};
-		titles[] = {};
-	};
-	
-	class twc_sound_airraid_warning {
-		name = "airraid_warning";
-		sound[] = {"twc_flavour\sounds\alarms\air_raid_warning.ogg", 1, 1};
-		titles[] = {};
-	};
-	
-	class twc_sound_airraid_allclear {
-		name = "airraid_allclear";
-		sound[] = {"twc_flavour\sounds\alarms\air_raid_allclear.ogg", 1, 1};
-		titles[] = {};
-	};
-	
-	class twc_sound_radio_norsk {
-		name = "radio_norsk";
-		sound[] = {"twc_flavour\sounds\radio\radio_norsk.ogg", 1, 1};
-		titles[] = {};
-	};
+	ADD_SOUND(airraid_warning,alarms);
+	ADD_SOUND(airraid_allclear,alarms);
 };
