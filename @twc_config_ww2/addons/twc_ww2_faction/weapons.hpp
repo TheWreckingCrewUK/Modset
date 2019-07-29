f9f19146a30df58e659625c0ca065fecfc940718
Neciota <neciota@gmail.com>
2019-07-29 16:25:46 +0200

    Sten fix
    IFA3 open bolt mechanic fucked it up, but they'll fix it in the next
    patch. Until then, this disables the mechanic.

---
+++ @twc_config_ww2/addons/twc_ww2_faction/weapons.hpp
@@ -0,0 +1,8 @@
+//Temporary Removal of IFA3 Open bolt system to prevent dragging bug with Sten.
+class LIB_SMG;
+class LIB_Sten_Mk2: LIB_SMG {
+	WW2_boltSnapSound = "";
+};
+class LIB_Sten_Mk5: LIB_Sten_Mk2 {
+	WW2_boltSnapSound = "";
+};
