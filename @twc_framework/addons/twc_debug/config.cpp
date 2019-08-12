26619816b0a68d0b596edddbfd4afe1504018eae
Bosenator <strooptroopers+ghbose@gmail.com>
2019-08-12 15:37:14 +0100

    add bunch of debug functions

---
+++ @twc_framework/addons/twc_debug/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches {
+	class TWC_Debug {
+		units[]={};
+		weapons[]={};
+		requiredVersion = 1.7;
+		requiredAddons[] = {"TWC_Core"};
+		author[] = {};
+		authorUrl = "";
+		version = "1";
+		versionStr = "1";
+		versionAr[] = {1};
+	};
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgWeapons.hpp"
