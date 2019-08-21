812f2267fd6b85f95edd99ea8ebc7ed6f2981af0
Neciota <neciota@gmail.com>
2019-08-21 18:48:00 +0200

    Mallas Units
    All the units Mallas requested

---
+++ @twc_config/addons/twc_opfor_plan/groups.hpp
@@ -0,0 +1,148 @@
+class TWC_Groups_PLAN_Regular
+{
+	name = "Infantry";
+	class Section_Mechanised
+	{
+		name = "Section (Mechanised)";
+		faction = "TWC_PLAN";
+		side = 2;
+		class Unit0
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_SectionCommander";
+			rank = "SERGEANT";
+			position[] = {0,0,0};
+		};
+		class Unit1
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Rifleman";
+			rank = "PRIVATE";
+			position[] = {2,0,0};
+		};
+		class Unit2
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Grenadier";
+			rank = "PRIVATE";
+			position[] = {4,0,0};
+		};
+		class Unit3
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Rifleman_AT";
+			rank = "PRIVATE";
+			position[] = {6,0,0};
+		};
+		class Unit4
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Autorifleman";
+			rank = "PRIVATE";
+			position[] = {8,0,0};
+		};
+		class Unit5
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_2iC";
+			rank = "CORPORAL";
+			position[] = {10,0,0};
+		};
+		class Unit6
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_MG";
+			rank = "PRIVATE";
+			position[] = {12,0,0};
+		};
+		class Unit7
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Rifleman_AT";
+			rank = "PRIVATE";
+			position[] = {14,0,0};
+		};
+		class Unit8
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Commander";
+			rank = "CORPORAL";
+			position[] = {16,0,0};
+		};
+		class Unit9
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Crew";
+			rank = "PRIVATE";
+			position[] = {18,0,0};
+		};
+		class Unit11
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Crew";
+			rank = "PRIVATE";
+			position[] = {20,0,0};
+		};
+	};
+	class Platoon_Command
+	{
+		name = "Platoon Command";
+		faction = "TWC_PLAN";
+		side = 2;
+		class Unit0
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Platoon_Commander";
+			rank = "MAJOR";
+			position[] = {0,0,0};
+		};
+		class Unit1
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Platoon_Sergeant";
+			rank = "LIEUTENANT";
+			position[] = {2,0,0};
+		};
+		class Unit2
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Platoon_Medic";
+			rank = "PRIVATE";
+			position[] = {4,0,0};
+		};
+		class Unit3
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_FAC";
+			rank = "MAJOR";
+			position[] = {6,0,0};
+		};
+	};
+	class Vehicle_Crew
+	{
+		name = "Vehicle Crew";
+		faction = "TWC_PLAN";
+		side = 2;
+		class Unit0
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Commander";
+			rank = "SERGEANT";
+			position[] = {0,0,0};
+		};
+		class Unit1
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Crew";
+			rank = "PRIVATE";
+			position[] = {2,0,0};
+		};
+		class Unit2
+		{
+			side = 2;
+			vehicle = "TWC_Infantry_PLAN_Regular_Vehicle_Crew";
+			rank = "PRIVATE";
+			position[] = {4,0,0};
+		};
+	};
+};
