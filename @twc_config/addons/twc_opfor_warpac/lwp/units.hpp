4b8f4ff2a1c920a4faabd3eabe0c81f99ec0d344
Neciota <neciota@gmail.com>
2019-09-24 21:41:24 +0200

    NVA Units

---
+++ @twc_config/addons/twc_opfor_warpac/lwp/units.hpp
@@ -0,0 +1,16 @@
+class TWC_OPFOR_WARPAC_LWP_Base: O_Soldier_base_F {
+	author = "[TWC] Rik";
+	scope = 1;
+	displayName = "Soldier Base";
+	faction = "TWC_OPFOR_Units_LWP";
+	vehicleClass = "EdSubcat_Personnel";
+	nakedUniform = "U_BasicBody";
+	uniformClass = "TWC_Uniform_LWP_Moro";
+	genericNames = "CUP_Names_GermanMen";
+	backpack = "";
+	identityTypes[] = {
+		"LanguageGRE_F",
+		"Head_Euro",
+		"G_GUERIL_default"
+	};
+};
---
+++ @twc_config/addons/twc_opfor_warpac/nva/groups.hpp
@@ -0,0 +1,604 @@
+class TWC_Groups_WARPAC_NVA {
+	name = "National People's Army";
+	class Infantry {
+		name = "Infantry";
+		class Group {
+			name = "Group";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_RPG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSRPG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_DGL";
+				rank = "CORPORAL";
+				position[] = {8,0,0};
+			};
+			class Unit5 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+			class Unit6 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Rifleman";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class MG_Team {
+			name = "MG Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MG";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+		};
+		class AA_Team {
+			name = "AA Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+		};
+		class AT_Team {
+			name = "AT Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AT";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+		};
+	};
+	class Motorised_Infantry {
+		name = "Motorised Infantry";
+		class Group {
+			name = "Group";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_RPG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSRPG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_DGL";
+				rank = "CORPORAL";
+				position[] = {8,0,0};
+			};
+			class Unit5 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+			class Unit6 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Rifleman";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+			class Unit7 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Ural";
+				rank = "PRIVATE";
+				position[] = {18,0,0};
+			};
+		};
+		class MG_Team {
+			name = "MG Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MG";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Ural";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class AA_Team {
+			name = "AA Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Ural";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+		};
+		class AT_Team {
+			name = "AT Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AT";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Ural";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+	};
+	class Mechanised_Infantry {
+		name = "Mechanised Infantry";
+		class Group_BTR {
+			name = "Group (BTR)";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_RPG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSRPG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_DGL";
+				rank = "CORPORAL";
+				position[] = {8,0,0};
+			};
+			class Unit5 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+			class Unit6 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Rifleman";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+			class Unit7 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BTR60";
+				rank = "PRIVATE";
+				position[] = {18,0,0};
+			};
+		};
+		class Group_BMP {
+			name = "Group (BMP)";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_RPG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSRPG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_DGL";
+				rank = "CORPORAL";
+				position[] = {8,0,0};
+			};
+			class Unit5 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AR";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+			class Unit6 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_Rifleman";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+			class Unit7 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BMP1";
+				rank = "PRIVATE";
+				position[] = {18,0,0};
+			};
+		};
+		class MG_Team_BTR {
+			name = "MG Team (BTR)";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MG";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BTR60";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class MG_Team_BMP1 {
+			name = "MG Team (BMP)";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MG";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSMG";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BMP1";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class AA_Team {
+			name = "AA Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AA";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MTLB";
+				rank = "PRIVATE";
+				position[] = {10,0,0};
+			};
+		};
+		class AT_Team {
+			name = "AT Team";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_GL";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_AT";
+				rank = "PRIVATE";
+				position[] = {2,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {4,0,0};
+			};
+			class Unit3 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_ASSAT";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit4 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_MTLB";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+	};
+	class Armour {
+		name = "Armour";
+		class T72_Platoon {
+			name = "T-72 Platoon";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T72";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T72";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T72";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class T55_Platoon {
+			name = "T-55 Platoon";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T55";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T55";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_T55";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+		class BRDM2_Platoon {
+			name = "SPW-40P2 Platoon";
+			faction = "TWC_OPFOR_Units_NVA";
+			side = 0;
+			class Unit0 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BRDM";
+				rank = "SERGEANT";
+				position[] = {0,0,0};
+			};
+			class Unit1 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BRDM";
+				rank = "PRIVATE";
+				position[] = {6,0,0};
+			};
+			class Unit2 {
+				side = 0;
+				vehicle = "TWC_OPFOR_WARPAC_NVA_BRDM";
+				rank = "PRIVATE";
+				position[] = {12,0,0};
+			};
+		};
+	};
+};
