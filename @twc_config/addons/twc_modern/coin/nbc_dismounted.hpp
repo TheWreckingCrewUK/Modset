f2230dd4583742248ae3de5b9c649fee34b89cb8
Neciota <neciota@gmail.com>
2019-10-02 22:08:42 +0200

    Modern NBC Units

---
+++ @twc_config/addons/twc_modern/coin/nbc_dismounted.hpp
@@ -0,0 +1,64 @@
+//COIN, NBC, DISMOUNTED
+//Section
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Rifleman: TWC_Infantry_Modern_COIN_NBC_Rifleman
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Pointman";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Grenadier: TWC_Infantry_Modern_COIN_NBC_Grenadier
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Grenadier";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Autorifleman: TWC_Infantry_Modern_COIN_NBC_Autorifleman
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Autorifleman";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_MG: TWC_Infantry_Modern_COIN_NBC_MG
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_MG";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Marksman: TWC_Infantry_Modern_COIN_NBC_Marksman
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Marksman";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_SectionCommander: TWC_Infantry_Modern_COIN_NBC_SectionCommander
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_SectionCommander";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_2iC: TWC_Infantry_Modern_COIN_NBC_2iC
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_2iC";
+};
+
+//Platoon
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Platoon_Commander: TWC_Infantry_Modern_COIN_NBC_Platoon_Commander
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Platoon_Commander";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Platoon_Sergeant: TWC_Infantry_Modern_COIN_NBC_Platoon_Sergeant
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Platoon_Sergeant";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Platoon_Medic: TWC_Infantry_Modern_COIN_NBC_Platoon_Medic
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Platoon_Medic";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_Platoon_Mortar: TWC_Infantry_Modern_COIN_NBC_Platoon_Mortar
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Platoon_Mortar";
+};
+class TWC_Infantry_Modern_COIN_Dismounted_NBC_CSM: TWC_Infantry_Modern_COIN_NBC_CSM
+{
+	CATEGORY(TWC_Infantry_COIN_Dismounted_NBC)
+	backpack = "TWC_Backpack_Modern_COIN_Dismounted_Woodland_Platoon_Sergeant";
+};
