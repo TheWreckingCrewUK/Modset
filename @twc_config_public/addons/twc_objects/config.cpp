f579c3b514c17f3fdc64555510adddb620457e46
Hobbnob <thehobbnob@gmail.com>
2020-01-20 01:24:40 +0000

    public defenses
    for the ghetto PB building system

--- @twc_config_public/addons/twc_objects/config.cpp
+++ @twc_config_public/addons/twc_objects/config.cpp
@@ -46,6 +46,9 @@ class CfgVehicles {
 	class Land_BagFence_Round_F;
 	class Land_BagBunker_Small_F;
 	class Land_File_research_F;
+	class Land_HBarrier_5_F;
+	class Land_HBarrierTower_F;
+	class Land_HBarrier_Big_F;
 	class FlagSmall_F;
 	
 	class twc_flag_eod: FlagSmall_F {
@@ -72,6 +75,24 @@ class CfgVehicles {
 	};
 
 	class twc_Land_BagFence_Long_F: Land_BagFence_Long_F {
+		class EventHandlers: EventHandlers {
+			init = "[(_this select 0),true,[0,1,0],0] call ace_dragging_fnc_setCarryable;";
+		};
+	};
+	
+	class twc_Land_HBarrier_5_F: Land_HBarrier_5_F {
+		class EventHandlers: EventHandlers {
+			init = "[(_this select 0),true,[0,1,0],0] call ace_dragging_fnc_setCarryable;";
+		};
+	};
+
+	class twc_Land_HBarrierTower_F: Land_HBarrierTower_F {
+		class EventHandlers: EventHandlers {
+			init = "[(_this select 0),true,[0,1,0],0] call ace_dragging_fnc_setCarryable;";
+		};
+	};
+
+	class twc_Land_HBarrier_Big_F: Land_HBarrier_Big_F {
 		class EventHandlers: EventHandlers {
 			init = "[(_this select 0),true,[0,1,0],0] call ace_dragging_fnc_setCarryable;";
 		};
