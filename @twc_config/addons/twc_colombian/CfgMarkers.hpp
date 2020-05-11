630040d099a78a547b032914ce3a23f31d21807a
Sargers <sargers12@gmail.com>
2020-05-11 16:54:03 +0200

    Added colombian flags

---
+++ @twc_config/addons/twc_colombian/CfgMarkers.hpp
@@ -0,0 +1,25 @@
+class CfgMarkers {
+	class flag;
+
+	class twc_colombia_flag: Flag {
+		name = "Colombia";
+		icon = "twc_colombian\data\colombia_flag.paa";
+		texture = "twc_colombian\data\colombia_flag.paa";
+		size = 32;
+		scope = 2;
+		shadow = 0;
+		color[] = {1,1,1,1};
+		markerClass = "Flags";
+	};
+
+	class twc_FARC_flag: Flag {
+		name = "Revolutionary Armed Forces of Colombia";
+		icon = "twc_colombian\data\FARC_flag.paa";
+		texture = "twc_colombian\data\FARC_flag.paa";
+		size = 32;
+		scope = 2;
+		shadow = 0;
+		color[] = {1,1,1,1};
+		markerClass = "Flags";
+	};
+};
