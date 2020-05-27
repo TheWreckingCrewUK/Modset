9b91f65345959061477e618a940e60a33819b1de
Neciota <neciota@gmail.com>
2020-05-27 23:48:30 +0200

    PLO

---
+++ @twc_config/addons/twc_opfor_Misc/compositions/coldwar_plo.hpp
@@ -0,0 +1,154 @@
+class TWC_Operation_Opfor_ColdWar_PLO {
+	name = "Palestine Liberation Organization";
+	
+	class TWC_Operation_Opfor_ColdWar_PLO_Section {
+		name = "Section"; //Name of Group
+		faction = "TWC_OPFOR_Units_PLO"; //Faction
+		side = 0; //Side
+		
+		class Unit0 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKS"; //Class name of desired solider
+			rank = "SERGEANT"; // Rank
+			position[] = {0,0,0}; //Position in the formation
+		};
+		class Unit1 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AK47"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {2,0,0}; //Position in the formation
+		};
+		class Unit2 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKM"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {4,0,0}; //Position in the formation
+		};
+		class Unit3 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_AT"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {6,0,0}; //Position in the formation
+		};
+		class Unit4 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Autorifleman"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {8,0,0}; //Position in the formation
+		};
+		class Unit5 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_M16"; //Class name of desired solider
+			rank = "CORPORAL"; // Rank
+			position[] = {10,0,0}; //Position in the formation
+		};
+		class Unit6 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Machinegunner"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {12,0,0}; //Position in the formation
+		};
+		class Unit7 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_Type56"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {14,0,0}; //Position in the formation
+		};
+		class Unit8 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Marksman"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {16,0,0}; //Position in the formation
+		};
+	};
+	class TWC_Operation_Opfor_ColdWar_PLO_MG_Team {
+		name = "MG Team"; //Name of Group
+		faction = "TWC_OPFOR_Units_Aden"; //Faction
+		side = 0; //Side
+		
+		class Unit0 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKS"; //Class name of desired solider
+			rank = "SERGEANT"; // Rank
+			position[] = {0,0,0}; //Position in the formation
+		};
+		class Unit1 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKM"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {2,0,0}; //Position in the formation
+		};
+		class Unit2 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_M16"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {4,0,0}; //Position in the formation
+		};
+		class Unit3 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Machinegunner"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {6,0,0}; //Position in the formation
+		};
+	};
+	class TWC_Operation_Opfor_ColdWar_PLO_RPG_Team {
+		name = "RPG Team"; //Name of Group
+		faction = "TWC_OPFOR_Units_Aden"; //Faction
+		side = 0; //Side
+		
+		class Unit0 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKS"; //Class name of desired solider
+			rank = "SERGEANT"; // Rank
+			position[] = {0,0,0}; //Position in the formation
+		};
+		class Unit1 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AK47"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {2,0,0}; //Position in the formation
+		};
+		class Unit2 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_AT"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {4,0,0}; //Position in the formation
+		};
+		class Unit3 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_AT"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {6,0,0}; //Position in the formation
+		};
+	};
+	class TWC_Operation_Opfor_ColdWar_PLO_Team {
+		name = "Rifle Team"; //Name of Group
+		faction = "TWC_OPFOR_Units_Aden"; //Faction
+		side = 0; //Side
+		
+		class Unit0 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_AKS"; //Class name of desired solider
+			rank = "SERGEANT"; // Rank
+			position[] = {0,0,0}; //Position in the formation
+		};
+		class Unit1 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Rifleman_Type56"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {2,0,0}; //Position in the formation
+		};
+		class Unit2 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Marksman"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {4,0,0}; //Position in the formation
+		};
+		class Unit3 {
+			side = 0;
+			vehicle = "TWC_OPFOR_PLO_Autorifleman"; //Class name of desired solider
+			rank = "PRIVATE"; // Rank
+			position[] = {6,0,0}; //Position in the formation
+		};
+	};
+};
