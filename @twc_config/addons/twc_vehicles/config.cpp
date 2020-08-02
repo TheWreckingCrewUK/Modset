d9974e97045b14f504a47586515f41c4d8b99e65
Justin Katulski <jkatulski96@gmail.com>
2020-08-02 13:21:22 -0400

    edited out burnes challenger references

--- @twc_config/addons/twc_vehicles/config.cpp
+++ @twc_config/addons/twc_vehicles/config.cpp
@@ -81,9 +81,10 @@ class CfgPatches {
 			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Desert_COIN",
 			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Desert",
 			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Woodland_COIN",
-			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Woodland",
-			"TWC_Vehicle_Modern_Challenger2_Desert",
-			"TWC_Vehicle_Modern_Challenger2_Woodland"
+			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Woodland"
+			
+//			"TWC_Vehicle_Modern_Challenger2_Woodland",
+//			"TWC_Vehicle_Modern_LandRoverWMIK_HMG_Woodland"
 		};
 
 		weapons[] = {
@@ -231,7 +232,7 @@ class CfgVehicles {
 	#include "vehicles\lr_snatch.hpp"
 	#include "vehicles\lr_wmik.hpp"
 	#include "vehicles\wildcat.hpp"
-	#include "vehicles\challenger2.hpp"
+//	#include "vehicles\challenger2.hpp"
 	
 	#include "generic\generic.hpp"
 	#include "coldwar\coldwar.hpp"
