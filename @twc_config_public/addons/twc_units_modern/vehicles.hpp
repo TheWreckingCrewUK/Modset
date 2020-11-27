827aecf0f48a5cc81683d222a436a099ac1d7929
Hobbnob <thehobbnob@gmail.com>
2020-11-27 00:19:30 +0000

    husky physics, cup btr physics

--- @twc_config_public/addons/twc_units_modern/vehicles.hpp
+++ @twc_config_public/addons/twc_units_modern/vehicles.hpp
@@ -1181,13 +1181,13 @@
        {
            turnIncreaseConst  = 0.01; // basic sensitivity value, higher value = faster steering
            turnIncreaseLinear = 0.6; // higher value means less sensitive steering in higher speed, more sensitive in lower speeds
-           turnIncreaseTime   = 0.8; // higher value means smoother steering around the center and more sensitive when the actual steering angle gets closer to the max. steering angle
+           turnIncreaseTime   = 1.0; // higher value means smoother steering around the center and more sensitive when the actual steering angle gets closer to the max. steering angle
            
            turnDecreaseConst  = 0.2; // basic caster effect value, higher value = the faster the wheels align in the direction of travel
            turnDecreaseLinear = 0.5; // higher value means faster wheel re-centering in higher speed, slower in lower speeds
            turnDecreaseTime   = 0.8; // higher value means stronger caster effect at the max. steering angle and weaker once the wheels are closer to centered position
              
-           maxTurnHundred     = 0.1; // coefficient of the maximum turning angle @ 100km/h; limit goes linearly to the default max. turn. angle @ 0km/h
+           maxTurnHundred     = 0.2; // coefficient of the maximum turning angle @ 100km/h; limit goes linearly to the default max. turn. angle @ 0km/h
        };
 	};
 	
