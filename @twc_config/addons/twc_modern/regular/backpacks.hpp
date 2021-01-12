baabeb5cf9940f8edb1bafc77386d052e7746adb
TWC-Rik <18664259+TWC-Rik@users.noreply.github.com>
2021-01-12 23:44:35 +0100

    DAGR For Modern Arty Commander
    Map tools are for suckers.

--- @twc_config/addons/twc_modern/regular/backpacks.hpp
+++ @twc_config/addons/twc_modern/regular/backpacks.hpp
@@ -122,6 +122,7 @@ class TWC_Backpack_Modern_Regular_Woodland_Artillery_Commander: UK3CB_BAF_B_Berg
 	scopeArsenal = 1;
 	class TransportItems {
 		ADD_ITEM(ACRE_PRC117F,1);
+		ADD_ITEM(ACE_DAGR,1);
 	};
 };
 class TWC_Backpack_Modern_Regular_Woodland_FO: UK3CB_BAF_B_Bergen_MTP_SL_L_A
