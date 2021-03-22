064d1e9a3c85cd0a725786ad01b3f1185b41e587
rwalker959 <73700817+rwalker959@users.noreply.github.com>
2021-03-22 12:04:04 +0000

    Putting Modern Config back in to Joint Op Config

---
+++ @twc_config_jointop/addons/twc_modern/backpacks.hpp
@@ -0,0 +1,13 @@
+#define ADD_MAGA(a,b) class _nc_##a {magazine = a; count = b;}
+#define ADD_WEAP(a,b) class _nc_##a {weapon = a; count = b;}
+#define ADD_ITEM(a,b) class _nc_##a {name = a; count = b;}
+
+class UK3CB_BAF_B_Bergen_MTP_SL_L_A;
+class UK3CB_BAF_B_Bergen_MTP_JTAC_H_A;
+class UK3CB_BAF_B_Bergen_MTP_Medic_L_A;
+class UK3CB_BAF_B_Bergen_MTP_Rifleman_L_C;
+class UK3CB_BAF_B_Bergen_MTP_Rifleman_L_D;
+class UK3CB_BAF_B_Bergen_MTP_PointMan_L_A;
+
+#include "regular\backpacks.hpp"
+#include "coin\backpacks.hpp"
