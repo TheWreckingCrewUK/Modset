9f7d1d64d1cc5ed97c6aa7c8b496be446f9ec669
rwalker959 <73700817+rwalker959@users.noreply.github.com>
2021-06-14 10:46:20 +0100

    Update woodland.hpp

--- @twc_config/addons/twc_millennial/2000_coin/woodland.hpp
+++ @twc_config/addons/twc_millennial/2000_coin/woodland.hpp
@@ -71,14 +71,14 @@ class TWC_Infantry_2000_COIN_Woodland_Grenadier: TWC_Infantry_2000_Regular_Woodl
 	};
 		weapons[]=
 	{
-		"TWC_Weapon_L85A2_UGL_SUSAT3D",
+		"TWC_Weapon_L85A2_UGL_HWS_SUSAT3D",
 		"ACE_VMH3",
 		"Throw",
 		"Put"
 	};
 	respawnweapons[] =
 	{
-		"TWC_Weapon_L85A2_UGL_SUSAT3D",
+		"TWC_Weapon_L85A2_UGL_HWS_SUSAT3D",
 		"ACE_VMH3",
 		"Throw",
 		"Put"
