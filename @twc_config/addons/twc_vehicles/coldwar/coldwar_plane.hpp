29a1caed79863eaabc1d16a745fe434d4b13a90f
Rik <neciota@gmail.com>
2021-08-12 18:02:39 +0200

    C-47 Configuration

--- @twc_config/addons/twc_vehicles/coldwar/coldwar_plane.hpp
+++ @twc_config/addons/twc_vehicles/coldwar/coldwar_plane.hpp
@@ -28,3 +28,20 @@ class TWC_Vehicle_ColdWar_Hercules_RAF_VIV: TWC_Vehicle_Generic_Hercules_RAF_VIV
 
 	REGULAR_77({"crew"}, {"Intercom_2"})
 };
+
+class CUP_B_C47_USA;
+class TWC_Vehicle_ColdWar_C47_RAF: CUP_B_C47_USA {
+	scope = 2;
+	side = 1;
+	ADD_FACTION;
+
+	displayname = "Dakota (Cold War) - VIV (RAF)";
+	editorSubcategory = "TWC_ForceType_Airborne";
+
+	hiddenSelectionsTextures[] = {
+		"TWC_Vehicles\data\dc3_body_raf_01_co.paa",
+		"TWC_Vehicles\data\dc3_body_raf_02_co.paa"
+	};
+
+	REGULAR_77({"crew"}, {"Intercom_2"})
+};
