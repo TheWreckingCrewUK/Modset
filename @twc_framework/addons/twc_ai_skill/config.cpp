9d6e2403c20e445dda7a1f1410bfa02ba75570f1
Bosenator <Bosenator@users.noreply.github.com>
2022-03-07 11:27:37 +0000

    CfgAISkill
    Was too aggressive last time, as it predated weapon dispersion I think?

--- @twc_framework/addons/twc_ai_skill/config.cpp
+++ @twc_framework/addons/twc_ai_skill/config.cpp
@@ -26,10 +26,10 @@ class CfgVehicles {
 };
 
 class CfgAISkill {
-	aimingAccuracy[] = {0,   0, 1, 0.5};
+	aimingAccuracy[] = {0,   0, 1, 0.6};
 	aimingShake[]    = {0,   0, 1, 0.5};
-	aimingSpeed[]    = {0,   0, 1, 0.4};
-	commanding[]     = {0,   0, 1, 0.8};
+	aimingSpeed[]    = {0,   0, 1, 0.6};
+	commanding[]     = {0,   0, 1, 0.9};
 	courage[]        = {0, 0.3, 1,   1};
 	general[]        = {0,   0, 1,   1};
 	reloadSpeed[]    = {0, 0.5, 1,   1};
