d97cc0d52c8256378e8ed856f80dc18884f2cdb7
rwalker959 <rwalker959@gmail.com>
2022-03-10 10:34:12 +0000

    2020 Unit Loadouts

--- @twc_config/addons/twc_modern/2020_regular/nbc.hpp
+++ @twc_config/addons/twc_modern/2020_regular/nbc.hpp
@@ -2,42 +2,42 @@
 //Section
 class TWC_Infantry_2020_Regular_NBC_Rifleman: TWC_Infantry_2020_Regular_Woodland_Rifleman
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Grenadier: TWC_Infantry_2020_Regular_Woodland_Grenadier
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_MG: TWC_Infantry_2020_Regular_Woodland_MG
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Marksman: TWC_Infantry_2020_Regular_Woodland_Marksman
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_SectionCommander: TWC_Infantry_2020_Regular_Woodland_SectionCommander
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_2iC: TWC_Infantry_2020_Regular_Woodland_2iC
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -46,35 +46,35 @@ class TWC_Infantry_2020_Regular_NBC_2iC: TWC_Infantry_2020_Regular_Woodland_2iC
 //Platoon
 class TWC_Infantry_2020_Regular_NBC_Platoon_Commander: TWC_Infantry_2020_Regular_Woodland_Platoon_Commander
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Platoon_Sergeant: TWC_Infantry_2020_Regular_Woodland_Platoon_Sergeant
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Platoon_Medic: TWC_Infantry_2020_Regular_Woodland_Platoon_Medic
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Platoon_Mortar: TWC_Infantry_2020_Regular_Woodland_Platoon_Mortar
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_CSM: TWC_Infantry_2020_Regular_Woodland_CSM
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -83,21 +83,21 @@ class TWC_Infantry_2020_Regular_NBC_CSM: TWC_Infantry_2020_Regular_Woodland_CSM
 //Company
 class TWC_Infantry_2020_Regular_NBC_Company_Commander: TWC_Infantry_2020_Regular_Woodland_Company_Commander
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Company_2iC: TWC_Infantry_2020_Regular_Woodland_Company_2iC
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Company_Sergeant: TWC_Infantry_2020_Regular_Woodland_Company_Sergeant
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -106,28 +106,28 @@ class TWC_Infantry_2020_Regular_NBC_Company_Sergeant: TWC_Infantry_2020_Regular_
 //AT + AA
 class TWC_Infantry_2020_Regular_NBC_Javelin_Gunner: TWC_Infantry_2020_Regular_Woodland_Javelin_Gunner
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Javelin_Ass: TWC_Infantry_2020_Regular_Woodland_Javelin_Ass
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 /*class TWC_Infantry_2020_Regular_NBC_Starstreak_Gunner: TWC_Infantry_2020_Regular_Woodland_Starstreak_Gunner
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Starstreak_Ass: TWC_Infantry_2020_Regular_Woodland_Starstreak_Ass
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -136,7 +136,7 @@ class TWC_Infantry_2020_Regular_NBC_Starstreak_Ass: TWC_Infantry_2020_Regular_Wo
 //Sniper Team
 class TWC_Infantry_2020_Regular_NBC_Sniper: TWC_Infantry_2020_Regular_Woodland_Sniper
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -159,7 +159,7 @@ class TWC_Infantry_2020_Regular_NBC_Sniper: TWC_Infantry_2020_Regular_Woodland_S
 };
 class TWC_Infantry_2020_Regular_NBC_Spotter: TWC_Infantry_2020_Regular_Woodland_Spotter
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -184,7 +184,7 @@ class TWC_Infantry_2020_Regular_NBC_Spotter: TWC_Infantry_2020_Regular_Woodland_
 //Artillery Crew
 class TWC_Infantry_2020_Regular_NBC_Artillery_Commander: TWC_Infantry_2020_Regular_Woodland_Artillery_Commander
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -207,7 +207,7 @@ class TWC_Infantry_2020_Regular_NBC_FO: TWC_Infantry_2020_Regular_Woodland_FO
 //Aircraft
 class TWC_Infantry_2020_Regular_NBC_FAC: TWC_Infantry_2020_Regular_Woodland_FAC
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
@@ -223,14 +223,14 @@ class TWC_Infantry_2020_Regular_NBC_Helicopter_Pilot: TWC_Infantry_2020_Regular_
 //FVs
 class TWC_Infantry_2020_Regular_NBC_Vehicle_Commander: TWC_Infantry_2020_Regular_Woodland_Vehicle_Commander
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
 };
 class TWC_Infantry_2020_Regular_NBC_Vehicle_Crew: TWC_Infantry_2020_Regular_Woodland_Vehicle_Crew
 {
-	CATEGORY(TWC_Infantry_2020_Regular_NBC)
+	CATEGORY(TWC_Infantry_Regular_NBC)
 	uniformClass = "SP_NBCMk3_HoodUp_Item";
 	items[] += {"SP_Gasmask_S6"};
 	respawnItems[] += {"SP_Gasmask_S6"};
