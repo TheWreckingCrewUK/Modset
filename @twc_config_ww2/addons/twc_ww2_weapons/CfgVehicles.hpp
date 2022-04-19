f9dd16d451871584b7ee24623088ee723399be3a
rwalker959 <rwalker959@gmail.com>
2022-04-19 15:33:47 +0100

    6 Pounder Adjustments

--- @twc_config_ww2/addons/twc_ww2_weapons/CfgVehicles.hpp
+++ @twc_config_ww2/addons/twc_ww2_weapons/CfgVehicles.hpp
@@ -13,7 +13,7 @@ class CfgVehicles {
 		scope = 2;
 		scopeCurator = 2;
 		typicalCargo[] = {"TWC_Infantry_WW2_UK_Early_Woodland_Rifleman"};
-		side = 4;
+		side = 2;
 		displayName = "6 Pounder";
 		LIB_isTowable = 1;
 		faction = "TWC_WW2";
