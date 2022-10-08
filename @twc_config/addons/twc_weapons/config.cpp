566c8249c078db82cc15d6d837f3462977bbc96a
Rik <18664259+TWC-Rik@users.noreply.github.com>
2022-10-08 16:43:51 +0200

    L14A1 Carl Gustaf
    Adds a config for L14A1 Carl Gustaf and ammo types. Uses base game submunition systems instead of a script.

--- @twc_config/addons/twc_weapons/config.cpp
+++ @twc_config/addons/twc_weapons/config.cpp
@@ -6,6 +6,7 @@ class CfgPatches {
 		
 		requiredAddons[]= {
 			"A3_Weapons_F",
+			"cup_weapons_maaws",
 			"uk3cb_baf_weapons",
 			"uk3cb_baf_equipment",
 			"cba_xeh",
@@ -160,6 +161,45 @@ class CfgWeapons {
 			"TWC_Magazine_105mm_1rnd_Illum_IR"
 		};
 		ace_overpressure_damage = 0;
+	};
+	
+	class CUP_launch_MAAWS: Launcher_Base_F
+	{
+		class WeaponSlotsInfo;
+	};
+	
+	class TWC_Weapon_L14A1: CUP_launch_MAAWS
+	{
+		displayname = "L14A1";
+		magazines[] = {
+			"TWC_Magazine_L14A1_HEAT","TWC_Magazine_L14A1_HE","TWC_Magazine_L14A1_Smoke","TWC_Magazine_L14A1_Illum",
+			"TWC_Magazine_L14A1_HE_200", "TWC_Magazine_L14A1_Illum_200",
+			"TWC_Magazine_L14A1_HE_300", "TWC_Magazine_L14A1_Illum_300",
+			"TWC_Magazine_L14A1_HE_400", "TWC_Magazine_L14A1_Illum_400",
+			"TWC_Magazine_L14A1_HE_500", "TWC_Magazine_L14A1_Illum_500",
+			"TWC_Magazine_L14A1_HE_600", "TWC_Magazine_L14A1_Illum_600",
+			"TWC_Magazine_L14A1_HE_700", "TWC_Magazine_L14A1_Illum_700",
+			"TWC_Magazine_L14A1_HE_800", "TWC_Magazine_L14A1_Illum_800",
+			"TWC_Magazine_L14A1_HE_900", "TWC_Magazine_L14A1_Illum_900",
+			"TWC_Magazine_L14A1_HE_1000", "TWC_Magazine_L14A1_Illum_1000",
+			"TWC_Magazine_L14A1_HE_1100", "TWC_Magazine_L14A1_Illum_1100",
+			"TWC_Magazine_L14A1_HE_1200", "TWC_Magazine_L14A1_Illum_1200",
+			"TWC_Magazine_L14A1_HE_1300", "TWC_Magazine_L14A1_Illum_1300",
+			"TWC_Magazine_L14A1_HE_1400", "TWC_Magazine_L14A1_Illum_1400",
+			"TWC_Magazine_L14A1_HE_1500", "TWC_Magazine_L14A1_Illum_1500",
+			"TWC_Magazine_L14A1_HE_1600", "TWC_Magazine_L14A1_Illum_1600",
+			"TWC_Magazine_L14A1_HE_1700", "TWC_Magazine_L14A1_Illum_1700",
+			"TWC_Magazine_L14A1_HE_1800", "TWC_Magazine_L14A1_Illum_1800",
+			"TWC_Magazine_L14A1_HE_1900", "TWC_Magazine_L14A1_Illum_1900",
+			"TWC_Magazine_L14A1_HE_2000", "TWC_Magazine_L14A1_Illum_2000",
+			"TWC_Magazine_L14A1_HE_2100", "TWC_Magazine_L14A1_Illum_2100",
+			"TWC_Magazine_L14A1_HE_2200", "TWC_Magazine_L14A1_Illum_2200",
+			"TWC_Magazine_L14A1_HE_2300", "TWC_Magazine_L14A1_Illum_2300"
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass = 255;
+		};
 	};
 };
 
