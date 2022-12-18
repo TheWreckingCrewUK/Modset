71dabc0022e4111ec08e12d41dc4b956db61e5df
rwalker959 <rwalker959@gmail.com>
2022-12-18 18:42:22 +0000

    Modern SF Initial

---
+++ @twc_config/addons/twc_modern/modern_sf/units.hpp
@@ -0,0 +1,241 @@
+//REGULAR, WOODLAND
+//Section
+class TWC_Infantry_Modern_SF_Rifleman: TWC_Infantry_2020_Base
+{
+	scope = 2;
+	displayName = "Rifleman";
+	CATEGORY(TWC_Infantry_Modern_SF)
+	uniformClass = "CUP_I_B_PARA_Unit_1";
+	backpack = "TWC_Backpack_Modern_SF_Rifleman";
+	linkedItems[] =
+	{
+		"CFP_LBT6094_operator_OGA_OD",
+		"CUP_H_OpsCore_Covered_MTP_SF",
+		"ItemCompass",
+		"itemMap",
+		"ItemWatch"
+	};
+	respawnLinkedItems[] =
+	{
+		"CFP_LBT6094_operator_OGA_OD",
+		"CUP_H_OpsCore_Covered_MTP_SF",
+		"ItemCompass",
+		"itemMap",
+		"ItemWatch"
+	};
+	weapons[] =
+	{
+		"TWC_Weapon_C8_SF",
+		"TWC_Weapon_L105A2_SF",
+		"CUP_launch_M72A6",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	respawnweapons[] =
+	{
+		"TWC_Weapon_C8_SF",
+		"TWC_Weapon_L105A2_SF",
+		"CUP_launch_M72A6",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	magazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+	respawnmagazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+};
+class TWC_Infantry_Modern_SF_Grenadier: TWC_Infantry_Modern_SF_Rifleman
+{
+	displayName = "Grenadier";
+	backpack = "TWC_Backpack_Modern_SF_Grenadier";
+	weapons[]=
+	{
+		"TWC_Weapon_C8_SF_GL",
+		"TWC_Weapon_L105A2_SF",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	respawnweapons[] =
+	{
+		"TWC_Weapon_C8_SF_GL",
+		"TWC_Weapon_L105A2_SF",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	magazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_5("UK3CB_BAF_1Rnd_HE_Grenade_Shell"),
+		MAG_4("1Rnd_Smoke_Grenade_shell"),
+		MAG_2("1Rnd_SmokeGreen_Grenade_shell"),
+		MAG_2("1Rnd_SmokeRed_Grenade_shell"),
+		MAG_2("1Rnd_SmokeBlue_Grenade_shell"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+	respawnmagazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_5("UK3CB_BAF_1Rnd_HE_Grenade_Shell"),
+		MAG_4("1Rnd_Smoke_Grenade_shell"),
+		MAG_2("1Rnd_SmokeGreen_Grenade_shell"),
+		MAG_2("1Rnd_SmokeRed_Grenade_shell"),
+		MAG_2("1Rnd_SmokeBlue_Grenade_shell"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+};
+
+class TWC_Infantry_Modern_SF_Marksman: TWC_Infantry_Modern_SF_Rifleman
+{
+	displayName = "Marksman";
+	backpack = "TWC_Backpack_Modern_SF_Marksman";
+	weapons[] =
+	{
+		"TWC_Weapon_HK417_SF",
+		"TWC_Weapon_L105A2_SF",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	respawnweapons[] =
+	{
+		"TWC_Weapon_HK417_SF",
+		"TWC_Weapon_L105A2_SF",
+		"ACE_Vector",
+		"Throw",
+		"Put"
+	};
+	magazines[] =
+	{
+		MAG_8("20Rnd_762x51_Mag"),
+		MAG_4("20Rnd_762x51_Mag_Tracer"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+	respawnmagazines[] =
+	{
+		MAG_8("20Rnd_762x51_Mag"),
+		MAG_4("20Rnd_762x51_Mag_Tracer"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell")
+	};
+};
+class TWC_Infantry_Modern_SF_SectionCommander: TWC_Infantry_Modern_SF_Rifleman
+{
+	displayName = "Section Commander";
+	backpack = "TWC_Backpack_Modern_SF_SectionCommander";
+	TWC_isCommandRole = 1;
+};
+class TWC_Infantry_Modern_SF_2iC: TWC_Infantry_Modern_SF_Rifleman
+{
+	displayName = "Team Leader";
+	backpack = "TWC_Backpack_Modern_SF_2iC";
+	TWC_isCommandRole = 1;
+};
+
+class TWC_Infantry_Modern_SF_Medic: TWC_Infantry_Modern_SF_Rifleman
+{
+	displayName = "Team Medic";
+	backpack = "TWC_Backpack_Modern_SF_Medic";
+	attendant = 1;
+};
+
+//Aircraft
+class TWC_Infantry_Modern_SF_FAC: TWC_Infantry_Modern_SF_Grenadier
+{
+	displayName = "JTAC";
+	backpack = "TWC_Backpack_Modern_SF_FAC";
+	TWC_isCommandRole = 0;
+	weapons[] =
+	{
+		"TWC_Weapon_C8_SF_GL",
+		"TWC_Weapon_L105A2_SF",
+		"UK3CB_BAF_Soflam_Laserdesignator",
+		"Throw",
+		"Put"
+	};
+	respawnweapons[] =
+	{
+		"TWC_Weapon_C8_SF_GL",
+		"TWC_Weapon_L105A2_SF",
+		"UK3CB_BAF_Soflam_Laserdesignator",
+		"Throw",
+		"Put"
+	};
+	magazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_5("UK3CB_BAF_1Rnd_HE_Grenade_Shell"),
+		MAG_4("1Rnd_Smoke_Grenade_shell"),
+		MAG_2("1Rnd_SmokeGreen_Grenade_shell"),
+		MAG_2("1Rnd_SmokeRed_Grenade_shell"),
+		MAG_2("1Rnd_SmokeBlue_Grenade_shell"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell"),
+		"Laserbatteries"
+	};
+	respawnmagazines[] =
+	{
+		MAG_8("CUP_30Rnd_556x45_PMAG_BLACK_PULL"),
+		MAG_6("CUP_30Rnd_556x45_PMAG_BLACK_PULL_Tracer_Red"),
+		MAG_2("UK3CB_BAF_9_15Rnd"),
+		MAG_5("UK3CB_BAF_1Rnd_HE_Grenade_Shell"),
+		MAG_4("1Rnd_Smoke_Grenade_shell"),
+		MAG_2("1Rnd_SmokeGreen_Grenade_shell"),
+		MAG_2("1Rnd_SmokeRed_Grenade_shell"),
+		MAG_2("1Rnd_SmokeBlue_Grenade_shell"),
+		MAG_2("CUP_HandGrenade_L109A2_HE"),
+		MAG_2("ACE_M84"),
+		MAG_2("B_IR_Grenade"),
+		MAG_2("Chemlight_blue"),
+		MAG_2("SmokeShell"),
+		"Laserbatteries"
+	};
+};
+
--- @twc_config/addons/twc_modern/weapons.hpp
+++ @twc_config/addons/twc_modern/weapons.hpp
@@ -5,6 +5,12 @@ class UK3CB_BAF_L85A2_UGL;
 class UK3CB_BAF_L85A3;
 class UK3CB_BAF_L85A3_UGL;
 class UK3CB_BAF_L115A3_Ghillie;
+class UK3CB_BAF_L119A1_RIS;
+class UK3CB_BAF_L119A1_UKUGL;
+class arifle_SPAR_01_blk_f;
+class arifle_SPAR_03_blk_f;
+class arifle_SPAR_01_GL_blk_F;
+class UK3CB_BAF_L105A2;
 
 class TWC_Weapon_L85A2_RIS_ELCAN3D: UK3CB_BAF_L85A2_RIS {
 	scope = 1;
@@ -137,6 +143,72 @@ class TWC_Weapon_L115A3_Regular_Sniper: UK3CB_BAF_L115A3_Ghillie {
 		class LinkedItemsMuzzle {
 			slot = "MuzzleSlot";
 			item = "uk3cb_baf_silencer_l115a3";
+		};
+	};
+};
+class TWC_Weapon_C8_SF: arifle_SPAR_01_blk_F {
+	displayName = "C8";
+	scope = 1;
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot = "CowsSlot";
+			item = "cuo_optic_acog_ta01b_rmr_black_pip";
+		};
+		class LinkedItemsFrontSideRail {
+			slot = "PointerSlot";
+			item = "UK3CB_BAF_LLM_Flashlight_Black";
+		};
+		class LinkedItemsMuzzle {
+			slot = "MuzzleSlot";
+			item = "CUP_muzzle_snds_G36_black";
+		};
+	};
+};
+class TWC_Weapon_C8_SF_GL: arifle_SPAR_01_GL_blk_F {
+	displayName = "C8 GL";
+	scope = 1;
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot = "CowsSlot";
+			item = "cuo_optic_acog_ta01b_rmr_black_pip";
+		};
+		class LinkedItemsFrontSideRail {
+			slot = "PointerSlot";
+			item = "UK3CB_BAF_LLM_Flashlight_Black";
+		};
+		class LinkedItemsMuzzle {
+			slot = "MuzzleSlot";
+			item = "CUP_muzzle_snds_G36_black";
+		};
+	};
+};
+class TWC_Weapon_HK417_SF: arifle_SPAR_03_blk_f {
+	scope = 1;
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot = "CowsSlot";
+			item = "rksl_optic_pmii_312";
+		};
+		class LinkedItemsFrontSideRail {
+			slot = "PointerSlot";
+			item = "UK3CB_BAF_LLM_Flashlight_Black";
+		};
+		class LinkedItemsBipod {
+			slot = "UnderBarrelSlot";
+			item = "cup_bipod_vltor_modpod_black";
+		};
+		class LinkedItemsMuzzle {
+			slot = "MuzzleSlot";
+			item = "cup_muzzle_snds_socom762rc";
+		};
+	};
+};
+class TWC_Weapon_L105A2_SF: UK3CB_BAF_L105A2 {
+	scope = 1;
+	class LinkedItems {
+		class LinkedItemsMuzzle {
+			slot = "MuzzleSlot";
+			item = "UK3CB_BAF_Silencer_L105A1";
 		};
 	};
 };
