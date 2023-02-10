f8dee1a3cfc84f8ca3e5f74acf287ef67973d631
Justin Katulski <jkatulski96@gmail.com>
2023-02-09 21:12:43 -0500

    Increased CFGAI skill Floor
    This is part of a larger test. My hypothesis is that the randomness of our current ai skill is what is causing situations where and AI murders everyone in a building vs another that never turns around to face us.
    
    I've increase our camouflage in hopes of balancing enemy skill especially at range before we start firing.
    
    I'm very happy with the ace medical setting and ui changes so this is the next thing to tinker with

--- @twc_framework/addons/twc_ai_skill/config.cpp
+++ @twc_framework/addons/twc_ai_skill/config.cpp
@@ -19,22 +19,22 @@ class CfgVehicles {
 	
 	class B_Soldier_base_F: SoldierWB {
 		threat[] = { 1, 1, 1};
-		camouflage = 6;
+		camouflage = 4;
 		accuracy = 0.2;
 		cost = 10;
 	};
 };
 
 class CfgAISkill {
-	aimingAccuracy[] = {0,   0, 1, 0.6};
-	aimingShake[]    = {0,   0, 1, 0.5};
+	aimingAccuracy[] = {0, 0.1, 1, 0.6};
+	aimingShake[]    = {0, 0.1, 1, 0.5};
 	aimingSpeed[]    = {0,   0, 1, 0.6};
-	commanding[]     = {0,   0, 1, 0.9};
-	courage[]        = {0, 0.3, 1,   1};
+	commanding[]     = {0, 0.5, 1, 0.9};
+	courage[]        = {0,   1, 1,   1};
 	general[]        = {0,   0, 1,   1};
-	reloadSpeed[]    = {0, 0.5, 1,   1};
+	reloadSpeed[]    = {0,   1, 1,   1};
 	spotDistance[]   = {0,   1, 1,   1};
-	spotTime[]       = {0, 0.3, 1, 0.9};
+	spotTime[]       = {0, 0.5, 1, 0.9};
 };
 
 #include "CfgBrains.hpp"
