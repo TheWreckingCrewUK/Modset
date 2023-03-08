0b257199922ddc15f35d7a6915c7f21b6b43746e
Ross <73700817+rwalker959@users.noreply.github.com>
2023-03-08 09:38:47 +0000

    Move L96 Config to Weapons from Gear Fix 1

--- @twc_config/addons/twc_millennial/weapons.hpp
+++ @twc_config/addons/twc_millennial/weapons.hpp
@@ -6,6 +6,7 @@ class UK3CB_BAF_L85A2_UGL_HWS;
 class UK3CB_BAF_L110A2RIS;
 class UK3CB_BAF_L22;
 class UK3CB_BAF_L22A2;
+class UK3CB_BAF_L115A3;
 
 class TWC_Weapon_L86A1: UK3CB_BAF_L86A2 {
 	displayName = "L86A1";
@@ -112,6 +113,64 @@ class TWC_Weapon_L110A2_SUSAT3D: UK3CB_BAF_L110A2RIS {
 			item="UK3CB_BAF_SUSAT_3D";
 		};
 
+	};
+};
+class TWC_L96_W_Base: UK3CB_BAF_L115A3 {
+	scope = 1;
+	displayname = "L96A1";
+	magazines[] = {"UK3CB_BAF_762_L42A1_10Rnd", "UK3CB_BAF_762_L42A1_10Rnd_T"};
+	class WeaponSlotsInfo {
+		class MuzzleSlot {
+			class compatibleItems {};
+			displayName = "Muzzle";
+			iconPicture = "a3\weapons_f\Data\ui\attachment_muzzle";
+			iconPinpoint = "Center";
+			iconPosition[] = {0,0};
+			iconScale = 0;
+			linkProxy = "\a3\data_f\proxies\weapon_slots\MUZZLE";
+			scope = 0;
+		};
+		class CowsSlot {
+			class compatibleItems {
+				CUP_optic_Leupold_VX3 = 1;
+				cup_optic_leupoldmk4 = 1;
+				cup_optic_leupoldmk4_10x40_lrt_woodland = 1;
+				cup_optic_leupoldm3lr = 1;
+				cup_optic_sb_3_12x50_pmii = 1;
+				cup_optic_an_pvs_4 = 1;
+			};
+			displayName = "Optics Slot";
+			iconPicture = "a3\weapons_f\Data\ui\attachment_top";
+			iconPinpoint = "Bottom";
+			iconPosition[] = {0,0};
+			iconScale = 0;
+			linkProxy = "\A3\data_f\proxies\weapon_slots\TOP";
+			scope = 0;
+		};
+		mass = 140;
+	};
+};
+
+class TWC_L96_D_base: TWC_L96_W_Base {
+	hiddenSelections[] = {"camo","camo2","camo3"};
+	hiddenSelectionsTextures[] = {"\UK3CB_BAF_Weapons\addons\UK3CB_BAF_Weapons_L115\data\awcstock_FDE_co.paa","\UK3CB_BAF_Weapons\addons\UK3CB_BAF_Weapons_L115\data\AWC_Scope_co.paa","\UK3CB_BAF_Weapons\addons\UK3CB_BAF_Weapons_L115\data\Magnumbits_ca.paa"};
+	picture = "\UK3CB_BAF_Weapons\addons\UK3CB_BAF_Weapons_L115\data\gear_awmFDE_ca.paa";
+};
+
+class TWC_Weapon_L96_Woodland: TWC_L96_W_Base {
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot="CowsSlot";
+			item="cup_optic_leupoldmk4_10x40_lrt_woodland";
+		};
+	};
+};
+class TWC_Weapon_L96_Desert: twc_l96_D_Base {
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot="CowsSlot";
+			item="cup_optic_leupoldmk4";
+		};
 	};
 };
 
