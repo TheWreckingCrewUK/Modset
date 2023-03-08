b878fdc56805d566c24a4c28d9dbbd4d4faec585
Ross <73700817+rwalker959@users.noreply.github.com>
2023-03-08 13:37:29 +0000

    Add Macro to Backpack Code

--- @twc_config_public/addons/twc_modern/2010_coin_ana/backpacks.hpp
+++ @twc_config_public/addons/twc_modern/2010_coin_ana/backpacks.hpp
@@ -1,184 +1,84 @@
 class ANA_Backpack_Modern_terp: CUP_B_SLA_Medicbag
-	{
+{
 		scope = 1;
-		class TransportItems
-		{
-			class _xx_PRC_117F
-			{
-				name = "ACRE_PRC152";
-				count = 1;
-			};
-		};
+		scopeArsenal = 1;
+		class TransportItems {
+			ADD_ITEM(ACRE_PRC152,1);
 	};
+};
 
 	class ana_backpack_medic: CUP_B_SLA_Medicbag
-	{
+{
 		scope = 1;
-		class TransportItems
-		{
-            class _xx_Bandage
-			{
-				name = "ACE_fieldDressing";
-				count = 12;
-			};
-			class _xx_Morphine
-			{
-				name = "ACE_morphine";
-				count = 10;
-			};
-			class _xx_ACE_elasticBandage
-			{
-				name = "ACE_elasticBandage";
-				count = 15;
-			};
-			class _xxACE_quikclot
-			{
-				name = "ACE_quikclot";
-				count = 6;
-			};
-			class _xxACE_epinephrine
-			{
-				name = "ACE_epinephrine";
-				count = 10;
-			};
-			class _xxACE_packingBandage
-			{
-				name = "ACE_packingBandage";
-				count = 15;
-			};
-			class _xxACE_personalAidKit
-			{
-				name = "ACE_personalAidKit";
-				count = 1;
-			};
-			class _xxACE_salineIV_500
-			{
-				name = "ACE_salineIV_500";
-				count = 6;
-			};
-			class _xxACE_tourniquet
-			{
-				name = "ACE_tourniquet";
-				count = 4;
-			};
-		};
-		class TransportMagazines
-		{
-			class _xx_Smoke
-			{
-				magazine = "SmokeShell";
-				count = 4;
-			};
-		};
+		scopeArsenal = 1;
+	class TransportItems {
+		ADD_ITEM(ACE_fieldDressing,20);
+		ADD_ITEM(ACE_morphine,10);
+		ADD_ITEM(ACE_elasticBandage,20);
+		ADD_ITEM(ACE_quikclot,20);
+		ADD_ITEM(ACE_packingBandage,20);
+		ADD_ITEM(ACE_adenosine,4);
+		ADD_ITEM(ACE_atropine,4);
+		ADD_ITEM(ACE_epinephrine,10);
+		ADD_ITEM(ACE_salineIV_500,12);
+		ADD_ITEM(ACE_salineIV_250,4);
+		ADD_ITEM(ACE_tourniquet,4);
+		ADD_ITEM(ACE_personalAidKit,1);
+		ADD_ITEM(TWC_Item_Medical_SutureKit_25,1);
 	};
 
+	class TransportMagazines {
+		ADD_MAGA(SmokeShell, 4);
+	};
+};
 	
 	class ana_backpack_rpg: CUP_B_RPGPack_Khaki
 	{
 		scope = 1;
-		
-		class TransportMagazines
-		{
-			class _xx_og7
-			{
-				magazine = "CUP_OG7_M";
-				count = 3;
-			};
-			
-			class _xx_pg7
-			{
-				magazine = "CUP_PG7V_M";
-				count = 2;
-			};
-		};
-		
+		scopeArsenal = 1;
+{
+	class TransportMagazines {
+		ADD_MAGA(CUP_OG7_M, 3);
+		ADD_MAGA(CUP_PG7V_M,2);
 	};
+};
 	
 	class ana_backpack_mg: CUP_B_AlicePack_Khaki
 	{
 		scope = 1;
 		
-		class TransportMagazines
-		{
-			class _xx_pkm
-			{
-				magazine = "CUP_100Rnd_TE4_LRT4_762x54_PK_Tracer_Green_M";
-				count = 4;
-			};
-			
-		};
-		
+	class TransportMagazines {
+		ADD_MAGA(CUP_100Rnd_TE4_LRT4_762x54_PK_Tracer_Green_M, 4);
 	};
+		
+};
 	
-		class ana_backpack_leader: CUP_B_AlicePack_Khaki
+	class ana_backpack_leader: CUP_B_AlicePack_Khaki
 	{
 		scope = 1;
-		
-		class TransportItems
-		{
-			class _xx_PRC_117F
-			{
-				name = "ACRE_PRC117F";
-				count = 1;
-			};
-		};
-		class TransportMagazines
-		{
-			class _xx_Smoke
-			{
-				magazine = "SmokeShell";
-				count = 2;
-			};
-			class _xx_Smoke_Red
-			{
-				magazine = "SmokeShellRed";
-				count = 3;
-			};
-			class _xx_Smoke_Green
-			{
-				magazine = "SmokeShellGreen";
-				count = 3;
-			};
-			class _xx_Smoke_Blue
-			{
-				magazine = "SmokeShellBlue";
-				count = 3;
-			};
-		};
-		
+		scopeArsenal = 1;
+	class TransportItems {
+		ADD_ITEM(ACRE_PRC117F,1);
 	};
+	class TransportMagazines {
+		ADD_MAGA(SmokeShell, 2);
+		ADD_MAGA(SmokeShellRed, 3);
+		ADD_MAGA(SmokeShellGreen, 3);
+		ADD_MAGA(SmokeShellBlue, 3);
+		ADD_MAGA(SmokeShellBlue, 3);
+	};
+};
 	
-			class ana_backpack_2ic: UK3CB_BAF_B_Carryall_OLI
+	class ana_backpack_2ic: UK3CB_BAF_B_Carryall_OLI
 	{
 		scope = 1;
-		
-
-		class TransportMagazines
-		{
-			class _xx_mags
-			{
-				magazine = "CUP_30Rnd_762x39_AK47_M";
-				count = 10;
-			};
-			class _xx_Smoke
-			{
-				magazine = "SmokeShell";
-				count = 2;
-			};
-			class _xx_Smoke_Red
-			{
-				magazine = "SmokeShellRed";
-				count = 3;
-			};
-			class _xx_Smoke_Green
-			{
-				magazine = "SmokeShellGreen";
-				count = 3;
-			};
-			class _xx_Smoke_Blue
-			{
-				magazine = "SmokeShellBlue";
-				count = 3;
-			};
-		};
+		scopeArsenal = 1;
+	class TransportMagazines{
+		ADD_MAGA(CUP_30Rnd_762x39_AK47_M, 10);
+		ADD_MAGA(SmokeShell, 2);
+		ADD_MAGA(SmokeShellRed, 3);
+		ADD_MAGA(SmokeShellGreen, 3);
+		ADD_MAGA(SmokeShellBlue, 3);
+		ADD_MAGA(SmokeShellBlue, 3);	
+	};
  };
