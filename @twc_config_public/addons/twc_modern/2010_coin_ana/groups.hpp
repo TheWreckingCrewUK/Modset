26fe0b0245341f3e3c61e19eed24fb60fc83aff5
rwalker959 <rwalker959@gmail.com>
2023-03-09 10:34:38 +0000

    Update ANA Group

--- @twc_config_public/addons/twc_modern/2010_coin_ana/groups.hpp
+++ @twc_config_public/addons/twc_modern/2010_coin_ana/groups.hpp
@@ -6,7 +6,7 @@ class Modern_ANA_Section
        	class Unit0	
 	{
                 side = 1;
-                vehicle = "TWC_ANA_Commander"; //Class name of desired solider
+                vehicle = "twc_ana_commander"; //Class name of desired solider
                 rank = "CORPORAL"; // Rank
                 position[] = {0,3,0}; //Position in the formation
         };
@@ -27,42 +27,42 @@ class Modern_ANA_Section
         class Unit3
         {
                 side = 1;
-                vehicle = "TWC_ANA_Rifleman_AT";
+                vehicle = "twc_ana_rifleman_at";
                 rank = "PRIVATE";
                 position[] = {7,0,0};
         };
         class Unit4
         {
                 side = 1;
-                vehicle = "TWC_ANA_Subcommander";
+                vehicle = "twc_ana_subcommander";
                 rank = "PRIVATE";
                 position[] = {9,0,0};
         };
         class Unit5
         {
                 side = 1;
-                vehicle = "TWC_ANA_MG";
+                vehicle = "twc_ana_mg";
                 rank = "PRIVATE";
                 position[] = {11,0,0};
         };
         class Unit6
         {
                 side = 1;
-                vehicle = "TWC_ANA_MG_ASS";
+                vehicle = "twc_ana_mg_as";
                 rank = "PRIVATE";
                 position[] = {13,0,0};
         };
         class Unit7
         {
                 side = 1;
-                vehicle = "TWC_ANA_Medic";
+                vehicle = "twc_ana_medic";
                 rank = "PRIVATE";
                 position[] = {15,0,0};
         };
         class Unit8
         {
                 side = 1;
-                vehicle = "TWC_ANA_Interpreter";
+                vehicle = "twc_ana_interpreter";
                 rank = "PRIVATE";
         	position[] = {17,0,0};
 	};
